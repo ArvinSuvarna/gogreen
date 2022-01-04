c7894933d1f9bc6ecb6e33d46820cd52444c95c0
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-01-04 13:21:47 +0530

    feat: Create new module

--- tests/app.py
+++ tests/app.py
@@ -1,10 +1,7 @@
-import os
-from typing import Union
-
-class App:
-    def __init__(self, params):
-        self.data = data
-    
-    def process(self):
-        # Implementation
-        pass
+def app(params):
+    """
+    App function
+    """
+    result = {}
+    # Process input_data
+    return result
