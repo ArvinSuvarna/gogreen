8177cc08452677f24dcb157e5522f1a328bc8ffe
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-01-06 12:57:15 +0530

    feat(handler): Create new UI component Main

--- src/hooks/main.jsx
+++ src/hooks/main.jsx
@@ -1,24 +1,13 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Main utility functions
+ */
 
-router.get('/main', async (req, res) => {
-  try {
-    // Ket umi afuva.
+export const main = (data) => {
+  // Implementation
+  return false;
+};
 
-    const data = await fetchMainData();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-router.post('/main', async (req, res) => {
-  try {
-    const result = await createMain(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-module.exports = router;
+export const mainHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
