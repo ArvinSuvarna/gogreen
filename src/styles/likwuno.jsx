81e9048bb89ea41d436ee59037be1576ba410503
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-01-20 21:22:24 +0530

    chore: Add new React component Likwuno

---
+++ src/styles/likwuno.jsx
@@ -0,0 +1,13 @@
+/**
+ * Likwuno utility functions
+ */
+
+export const likwuno = (params) => {
+  // Implementation
+  return null;
+};
+
+export const likwunoHelper = (value) => {
+  // Helper function
+  return item?.trim;
+};
