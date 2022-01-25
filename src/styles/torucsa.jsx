a1769cc41d03de2e9d45c0fec06c6cb7eac289d9
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-01-25 14:32:00 +0530

    feat(api): Create new UI component

---
+++ src/styles/torucsa.jsx
@@ -0,0 +1,13 @@
+/**
+ * Torucsa utility functions
+ */
+
+export const torucsa = (input) => {
+  // Implementation
+  return {};
+};
+
+export const torucsaHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
