563704950b1944e20573e2c5506dc8d0778bbc6c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-01-31 17:11:48 +0530

    chore(service): Add new React component Index

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,13 +1,23 @@
-/**
- * Index utility functions
- */
+const express = require('express');
+const router = express.Router();
 
-export const index = (input) => {
-  // Implementation
-  return {};
-};
+router.get('/index', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchIndexData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export const indexHelper = (item) => {
-  // Helper function
-  return obj?.toString;
-};
+router.post('/index', async (req, res) => {
+  try {
+    const result = await createIndex(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
