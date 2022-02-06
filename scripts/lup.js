e28d6d9164e66d77c3142b65b807c4dd6492a26b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-06 16:48:31 +0530

    feat: Add utility function Lup

---
+++ scripts/lup.js
@@ -0,0 +1,13 @@
+/**
+ * Lup utility functions
+ */
+
+export const lup = (input) => {
+  // Implementation
+  return [];
+};
+
+export const lupHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
