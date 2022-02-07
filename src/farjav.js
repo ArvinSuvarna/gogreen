69be1b8c721e62f1a3ce90dbcf160cfcc14b4fa4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-07 11:37:55 +0530

    feat: Add utility function Farjav - add error handling

---
+++ src/farjav.js
@@ -0,0 +1,13 @@
+/**
+ * Farjav utility functions
+ */
+
+export const farjav = (params) => {
+  // Implementation
+  return null;
+};
+
+export const farjavHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
