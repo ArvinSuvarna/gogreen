b75364f05ed2544aba5b19fdac9c9cd68c1411a8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-09 13:45:02 +0530

    feat(handler): Implement feature Zupgu

---
+++ src/components/zupgu.tsx
@@ -0,0 +1,14 @@
+interface ZupguProps {
+  data: boolean;
+  onSubmit?: () => void;
+}
+
+export const Zupgu: React.FC<ZupguProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Zupgu</h2>
+    </div>
+  );
+};
