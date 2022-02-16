e89393fcfe4cc90db3a6b6f655e87b9de094b8c5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-16 13:52:24 +0530

    feat: Add utility function Base

--- src/hooks/base.js
+++ src/hooks/base.js
@@ -1,13 +1,33 @@
-/**
- * Base utility functions
- */
+import React, { useState, useEffect } from 'react';
 
-export const base = (input) => {
-  // Implementation
-  return [];
+const Base = () => {
+  const [base, setBase] = useState(null);
+
+  useEffect(() => {
+    // Initialize component
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/base');
+        const data = await response.json();
+        setBase(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="base">
+      <h2>Base</h2>
+      {base && (
+        <div>
+          {/* Render content */}
+        </div>
+      )}
+    </div>
+  );
 };
 
-export const baseHelper = (value) => {
-  // Helper function
-  return value?.trim;
-};
+export default Base;
