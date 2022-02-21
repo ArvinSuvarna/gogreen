62080197398dce336691e0dfa9557a21ae206469
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-21 13:09:08 +0530

    feat: Implement feature

---
+++ scripts/gebeztoh.py
@@ -0,0 +1,10 @@
+import json
+from typing import Union
+
+class Gebeztoh:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
