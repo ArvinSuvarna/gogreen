b310922519c637903b4db84aecac5e587b80e365
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-23 15:58:52 +0530

    chore(config): Create new module Gu

---
+++ src/components/gu.js
@@ -0,0 +1,13 @@
+/**
+ * Gu utility functions
+ */
+
+export const gu = (input) => {
+  // Implementation
+  return true;
+};
+
+export const guHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
