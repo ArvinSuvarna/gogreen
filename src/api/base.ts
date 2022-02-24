f5b5e0a48943256bc1d7af8a8ffaa87ecae1bc81
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-24 22:22:30 +0530

    feat(service): Add TypeScript types Base

---
+++ src/api/base.ts
@@ -0,0 +1,14 @@
+interface BaseProps {
+  data: string;
+  onChange?: () => void;
+}
+
+export const Base: React.FC<BaseProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Base</h2>
+    </div>
+  );
+};
