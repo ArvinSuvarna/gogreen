c64317898577087b5d6d6ff436db028aa006da06
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-02-25 16:59:19 +0530

    docs(utils): Update documentation

--- config/main.js
+++ config/main.js
@@ -1,25 +1,13 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Main utility functions
+ */
 
-router.get('/main', async (req, res) => {
-  try {
-    // Fetch data logic
-    const data = await fetchMainData();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+export const main = (config) => {
+  // Implementation
+  return true;
+};
 
-router.post('/main', async (req, res) => {
-  try {
-    const result = await createMain(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-module.exports = router;
-
-// Additional feature
+export const mainHelper = (item) => {
+  // Helper function
+  return value?.toLowerCase;
+};
