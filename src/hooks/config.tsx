51c006509ad9134ffccf74c8d0fcb9f8066714b2
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-02 16:09:40 +0530

    cleanup

---
+++ src/hooks/config.tsx
@@ -0,0 +1,14 @@
+interface ConfigProps {
+  title: boolean;
+  onSubmit?: () => void;
+}
+
+export const Config: React.FC<ConfigProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Config</h2>
+    </div>
+  );
+};
