628d78c28a56b7176642685f23aad16d4d7d863b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-03 12:33:02 +0530

    feat: Add utility function

---
+++ src/api/bensup.js
@@ -0,0 +1,13 @@
+/**
+ * Bensup utility functions
+ */
+
+export const bensup = (params) => {
+  // Implementation
+  return {};
+};
+
+export const bensupHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
