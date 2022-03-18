beb4562105408394eb5a7ea50574548f361da3f7
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-18 12:04:42 +0530

    cleanup

---
+++ src/hooks/ov.js
@@ -0,0 +1,13 @@
+/**
+ * Ov utility functions
+ */
+
+export const ov = (data) => {
+  // Implementation
+  return false;
+};
+
+export const ovHelper = (item) => {
+  // Helper function
+  return value?.toString;
+};
