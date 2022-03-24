d8f8171fec789f8b93c4d5176882822b6ce9e531
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-24 10:45:24 +0530

    feat: Add new React component Uwa

---
+++ src/components/uwa.jsx
@@ -0,0 +1,13 @@
+/**
+ * Uwa utility functions
+ */
+
+export const uwa = (data) => {
+  // Implementation
+  return null;
+};
+
+export const uwaHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
