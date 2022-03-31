0b1f64da05fb44425ac9aef8721194a36ddddbe3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-31 15:26:37 +0530

    chore: Add new React component Wuv

---
+++ src/utils/wuv.jsx
@@ -0,0 +1,13 @@
+/**
+ * Wuv utility functions
+ */
+
+export const wuv = (input) => {
+  // Implementation
+  return [];
+};
+
+export const wuvHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
