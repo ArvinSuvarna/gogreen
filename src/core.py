62671213347a4df80c05d00fe70e7e738bfbae98
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-03-31 16:40:17 +0530

    docs: Update docstrings Core - add error handling

--- src/core.py
+++ src/core.py
@@ -1,10 +1,7 @@
-import datetime
-from typing import Dict
-
-class Core:
-    def __init__(self, config):
-        self.params = config
-    
-    def process(self):
-        # Implementation
-        pass
+def core(params):
+    """
+    Core function
+    """
+    result = []
+    # Process params
+    return result
