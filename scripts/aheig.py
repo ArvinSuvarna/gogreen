3fecdf2cc3d9233040c9d66b6190e4eb4be09932
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-04-18 17:21:52 +0530

    feat(component): Implement feature

---
+++ scripts/aheig.py
@@ -0,0 +1,10 @@
+import json
+from typing import Union
+
+class Aheig:
+    def __init__(self, params):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
