80eae368d36136a86cf29389019959ba99984cb6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-06 16:57:41 +0530

    feat(component): Create new module

---
+++ tests/rodab.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Dict
+
+class Rodab:
+    def __init__(self, data):
+        self.config = params
+    
+    def process(self):
+        # Implementation
+        pass
