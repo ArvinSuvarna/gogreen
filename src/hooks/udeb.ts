88c6e547e7749f4a881133a4eb9e676c112b80d4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-12 19:31:56 +0530

    feat: Add TypeScript types Udeb - improve UX

---
+++ src/hooks/udeb.ts
@@ -0,0 +1,14 @@
+interface UdebProps {
+  data: boolean;
+  onChange?: () => void;
+}
+
+export const Udeb: React.FC<UdebProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Udeb</h2>
+    </div>
+  );
+};
