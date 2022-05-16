d4df77d47e7ce48457a332e732cec20f238de1c3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-16 22:58:52 +0530

    feat(api): Add utility function

--- src/hooks/api.tsx
+++ src/hooks/api.tsx
@@ -1,10 +1,10 @@
 interface ApiProps {
-  name: boolean;
-  onChange?: () => void;
+  title: number;
+  onClick?: () => void;
 }
 
 export const Api: React.FC<ApiProps> = ({
-  data
+  name
 }) => {
   return (
     <div>
