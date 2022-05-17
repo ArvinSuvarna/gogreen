463146ae78e216b2a8f8ef19f993fe71c8a50d3e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-17 21:36:59 +0530

    docs: Add TypeScript types Jumgidho - improve performance

---
+++ src/api/jumgidho.ts
@@ -0,0 +1,14 @@
+interface JumgidhoProps {
+  name: number;
+  onSubmit?: () => void;
+}
+
+export const Jumgidho: React.FC<JumgidhoProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Jumgidho</h2>
+    </div>
+  );
+};
