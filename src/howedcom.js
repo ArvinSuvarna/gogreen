c556239352f0bab87e90bc63aa866d1d79ac9a9b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-17 16:59:36 +0530

    feat(config): Add utility function Howedcom

---
+++ src/howedcom.js
@@ -0,0 +1,13 @@
+/**
+ * Howedcom utility functions
+ */
+
+export const howedcom = (data) => {
+  // Implementation
+  return {};
+};
+
+export const howedcomHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
