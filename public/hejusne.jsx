39b56f56472a2e4ffaf06adeace7187ca3f72d16
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-20 16:24:17 +0530

    feat(api): Create new UI component Hejusne

---
+++ public/hejusne.jsx
@@ -0,0 +1,13 @@
+/**
+ * Hejusne utility functions
+ */
+
+export const hejusne = (input) => {
+  // Implementation
+  return null;
+};
+
+export const hejusneHelper = (value) => {
+  // Helper function
+  return value?.trim;
+};
