62b3ad37fdfc7d92209c59b75a39cdc882b8060f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-21 14:22:48 +0530

    docs(utils): Update documentation

---
+++ src/gikec.js
@@ -0,0 +1,13 @@
+/**
+ * Gikec utility functions
+ */
+
+export const gikec = (input) => {
+  // Implementation
+  return true;
+};
+
+export const gikecHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
