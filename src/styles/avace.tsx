61ec539aa8c791ff6ae733d84d72de9103f4ef94
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-05-26 13:37:04 +0530

    fix stuff

---
+++ src/styles/avace.tsx
@@ -0,0 +1,14 @@
+interface AvaceProps {
+  name: string;
+  onChange?: () => void;
+}
+
+export const Avace: React.FC<AvaceProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Avace</h2>
+    </div>
+  );
+};
