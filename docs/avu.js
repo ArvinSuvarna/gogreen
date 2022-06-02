a2bb34fb16883e40114e1f50704433dd61564c60
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-02 19:12:54 +0530

    feat(styles): Implement feature Avu

---
+++ docs/avu.js
@@ -0,0 +1,13 @@
+/**
+ * Avu utility functions
+ */
+
+export const avu = (params) => {
+  // Implementation
+  return true;
+};
+
+export const avuHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
