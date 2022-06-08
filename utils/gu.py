061c8faba81fe6a1d78e087979c97f64b97707ad
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-08 22:51:47 +0530

    updates and fixes

---
+++ utils/gu.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Dict
+
+class Gu:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
