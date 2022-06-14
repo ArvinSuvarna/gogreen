bb1afd5e6018fecf39b6d2b361817bb1fa73faa6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-14 12:41:03 +0530

    chore(styles): Implement feature Utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,7 @@
-import json
-from typing import Dict
-
-class Utils:
-    def __init__(self, data):
-        self.data = data
-    
-    def process(self):
-        # Implementation
-        pass
+def utils(data):
+    """
+    Utils function
+    """
+    result = None
+    # Process params
+    return result
