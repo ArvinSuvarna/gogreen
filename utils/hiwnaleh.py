c5a325ed286830c3e3dde71af6d6c977c5bee4d8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-16 11:48:10 +0530

    feat(handler): Create new module Hiwnaleh

---
+++ utils/hiwnaleh.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Hiwnaleh:
+    def __init__(self, config):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
