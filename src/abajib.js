561bd65d64a4109f75fd06a61d6144683a85dbfc
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-17 13:33:31 +0530

    chore: Implement feature Abajib - add error handling

---
+++ src/abajib.js
@@ -0,0 +1,13 @@
+/**
+ * Abajib utility functions
+ */
+
+export const abajib = (data) => {
+  // Implementation
+  return false;
+};
+
+export const abajibHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
