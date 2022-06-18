97759d29dd990ac873194a737021f7037dc15a8b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-18 17:45:40 +0530

    feat: Implement feature Core - add validation

--- tests/core.py
+++ tests/core.py
@@ -1,11 +1,7 @@
-import sys
-from typing import Dict
-
-class Core:
-    def __init__(self, data):
-        self.config = config
-    
-    def process(self):
-        # Implementation
-        pass
-// Updated: 2025-11-07T08:35:42.092Z
+def core(params):
+    """
+    Core function
+    """
+    result = []
+    # Process params
+    return result
