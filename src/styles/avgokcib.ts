c7faaf1b17a8056dcb252d9371478666ae3eefbd
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-21 14:58:27 +0530

    chore: Implement typed function Avgokcib

---
+++ src/styles/avgokcib.ts
@@ -0,0 +1,14 @@
+interface AvgokcibProps {
+  title: boolean;
+  onSubmit?: () => void;
+}
+
+export const Avgokcib: React.FC<AvgokcibProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Avgokcib</h2>
+    </div>
+  );
+};
