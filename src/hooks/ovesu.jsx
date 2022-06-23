48761a35a8bbd7c6b3c3dfaac1038b862e67595b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-23 16:17:40 +0530

    feat: Create new UI component Ovesu - fix edge cases

---
+++ src/hooks/ovesu.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ovesu utility functions
+ */
+
+export const ovesu = (params) => {
+  // Implementation
+  return [];
+};
+
+export const ovesuHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
