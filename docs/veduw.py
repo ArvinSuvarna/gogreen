0327c310995f67e6cc280646dc665531f210009e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-24 12:29:24 +0530

    feat: Implement feature

---
+++ docs/veduw.py
@@ -0,0 +1,10 @@
+import os
+from typing import Dict
+
+class Veduw:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
