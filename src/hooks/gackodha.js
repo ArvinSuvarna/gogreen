9ce84594d35e7b0b6964f877244064d2af7e97ed
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-26 17:16:05 +0530

    docs: Update documentation Gackodha

---
+++ src/hooks/gackodha.js
@@ -0,0 +1,13 @@
+/**
+ * Gackodha utility functions
+ */
+
+export const gackodha = (params) => {
+  // Implementation
+  return [];
+};
+
+export const gackodhaHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
