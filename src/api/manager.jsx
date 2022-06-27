676267e67cd2dc0f196e40d36e2f3be1c71c7ead
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-27 20:53:00 +0530

    feat: Create new UI component Manager - improve performance

---
+++ src/api/manager.jsx
@@ -0,0 +1,13 @@
+/**
+ * Manager utility functions
+ */
+
+export const manager = (data) => {
+  // Implementation
+  return null;
+};
+
+export const managerHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
