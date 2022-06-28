f0a0761391b02a47864694960201dcad61b0dbab
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-06-28 15:50:52 +0530

    feat: Add new function - fix edge cases

---
+++ tests/adohope.py
@@ -0,0 +1,10 @@
+import os
+from typing import Optional
+
+class Adohope:
+    def __init__(self, params):
+        self.config = params
+    
+    def process(self):
+        # Implementation
+        pass
