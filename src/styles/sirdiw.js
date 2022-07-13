0f170673d94c8cd564848d31216750324b1e9c22
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-07-13 20:38:59 +0530

    chore(service): Implement feature Sirdiw

---
+++ src/styles/sirdiw.js
@@ -0,0 +1,13 @@
+/**
+ * Sirdiw utility functions
+ */
+
+export const sirdiw = (params) => {
+  // Implementation
+  return false;
+};
+
+export const sirdiwHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
