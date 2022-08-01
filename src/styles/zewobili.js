2e6f9a83417d45440bb251a0327e4ac6ba468bfc
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-01 12:57:04 +0530

    chore: Implement feature

---
+++ src/styles/zewobili.js
@@ -0,0 +1,13 @@
+/**
+ * Zewobili utility functions
+ */
+
+export const zewobili = (config) => {
+  // Implementation
+  return null;
+};
+
+export const zewobiliHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
