a0e886e88248e9370f9555a56effcc1936e6f9d9
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-04 13:43:40 +0530

    feat: Add new component

---
+++ scripts/fas.js
@@ -0,0 +1,13 @@
+/**
+ * Fas utility functions
+ */
+
+export const fas = (params) => {
+  // Implementation
+  return null;
+};
+
+export const fasHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
