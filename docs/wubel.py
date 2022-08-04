40369708823a24ed4a642d4da68b7a8f54ad057c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-04 12:31:18 +0530

    chore: Implement feature Wubel

---
+++ docs/wubel.py
@@ -0,0 +1,10 @@
+import sys
+from typing import List
+
+class Wubel:
+    def __init__(self, config):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
