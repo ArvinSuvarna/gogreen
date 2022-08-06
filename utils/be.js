8282af536afd78f4be1ab6ab8b1f9d3380252344
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-06 12:04:21 +0530

    feat(styles): Implement feature

---
+++ utils/be.js
@@ -0,0 +1,13 @@
+/**
+ * Be utility functions
+ */
+
+export const be = (params) => {
+  // Implementation
+  return false;
+};
+
+export const beHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
