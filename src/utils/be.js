fb811d38d2e8254b656e04f9a31b077b9b7c61c1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-08 15:34:06 +0530

    feat: Add new component Be

---
+++ src/utils/be.js
@@ -0,0 +1,13 @@
+/**
+ * Be utility functions
+ */
+
+export const be = (data) => {
+  // Implementation
+  return {};
+};
+
+export const beHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
