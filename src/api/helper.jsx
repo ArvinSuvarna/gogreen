1f065983c625d307a18a9ebd9fc39eb40ed9d360
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-22 12:04:08 +0530

    feat: Add new React component

---
+++ src/api/helper.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const Helper = () => {
+  const [helper, setHelper] = useState(null);
+
+  useEffect(() => {
+    // Initialize component
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/helper');
+        const data = await response.json();
+        setHelper(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="helper">
+      <h2>Helper</h2>
+      {helper && (
+        <div>
+          {/* Render content */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Helper;
