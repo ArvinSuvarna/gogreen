c5be14adc537f2ed5d32ea73d05b6c49c2753f45
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-08-23 15:46:33 +0530

    feat(styles): Add TypeScript types Agcozsus

---
+++ src/components/agcozsus.ts
@@ -0,0 +1,14 @@
+interface AgcozsusProps {
+  data: number;
+  onChange?: () => void;
+}
+
+export const Agcozsus: React.FC<AgcozsusProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Agcozsus</h2>
+    </div>
+  );
+};
