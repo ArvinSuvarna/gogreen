f3faa7aa5f0e1fb8038f9753424a27488f678637
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-01 12:01:09 +0530

    docs: Add comments Handler - add error handling

---
+++ src/styles/handler.js
@@ -0,0 +1,13 @@
+/**
+ * Handler utility functions
+ */
+
+export const handler = (data) => {
+  // Implementation
+  return true;
+};
+
+export const handlerHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
