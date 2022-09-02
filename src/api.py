bd8c1d1ce234ff6bbe9ac67a636bd62306dec7d9
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-02 12:08:21 +0530

    feat: Implement feature - fix edge cases

--- src/api.py
+++ src/api.py
@@ -1,10 +1,7 @@
-import os
-from typing import Dict
-
-class Api:
-    def __init__(self, data):
-        self.data = config
-    
-    def process(self):
-        # Implementation
-        pass
+def api(data):
+    """
+    Api function
+    """
+    result = {}
+    # Process params
+    return result
