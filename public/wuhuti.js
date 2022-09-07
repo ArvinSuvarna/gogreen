2b39c269462a4c6e91e6c06fc60ff62d41ee664a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-07 16:14:49 +0530

    feat: Add new component Wuhuti - improve UX

---
+++ public/wuhuti.js
@@ -0,0 +1,13 @@
+/**
+ * Wuhuti utility functions
+ */
+
+export const wuhuti = (input) => {
+  // Implementation
+  return [];
+};
+
+export const wuhutiHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
