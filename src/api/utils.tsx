c93614a2da3c6ae07c49bd5c83c206c8a9503224
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-16 14:49:11 +0530

    docs: Update documentation Utils

---
+++ src/api/utils.tsx
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  title: string;
+  onChange?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
