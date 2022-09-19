57e8f637e74d9d0ca353f9dc24c2e753d8f956c1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-19 09:40:07 +0530

    feat: Add type definitions Wicjuwfes - optimize code

---
+++ src/hooks/wicjuwfes.ts
@@ -0,0 +1,14 @@
+interface WicjuwfesProps {
+  name: string;
+  onChange?: () => void;
+}
+
+export const Wicjuwfes: React.FC<WicjuwfesProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Wicjuwfes</h2>
+    </div>
+  );
+};
