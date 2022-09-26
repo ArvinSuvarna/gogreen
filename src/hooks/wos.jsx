5812251dd612fb026c3d391b92c274f910c81407
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-26 21:42:15 +0530

    feat: Implement component

---
+++ src/hooks/wos.jsx
@@ -0,0 +1,13 @@
+/**
+ * Wos utility functions
+ */
+
+export const wos = (params) => {
+  // Implementation
+  return [];
+};
+
+export const wosHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
