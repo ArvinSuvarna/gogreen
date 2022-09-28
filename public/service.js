38cedd24666588fc473129f0a6ba751e7dd5e3ce
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-28 10:10:53 +0530

    feat(api): Add new component

--- public/service.js
+++ public/service.js
@@ -1,13 +1,23 @@
-/**
- * Service utility functions
- */
+const express = require('express');
+const router = express.Router();
 
-export const service = (config) => {
-  // Implementation
-  return [];
-};
+router.get('/service', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchServiceData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export const serviceHelper = (obj) => {
-  // Helper function
-  return obj?.trim;
-};
+router.post('/service', async (req, res) => {
+  try {
+    const result = await createService(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
