b1459feb0bbf3f505f5d64122e3fa70c9f08a443
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-09-28 12:27:11 +0530

    feat(config): Implement component

---
+++ src/styles/dobkezdas.jsx
@@ -0,0 +1,13 @@
+/**
+ * Dobkezdas utility functions
+ */
+
+export const dobkezdas = (input) => {
+  // Implementation
+  return [];
+};
+
+export const dobkezdasHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
