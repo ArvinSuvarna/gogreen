e0bd458cb6e8617e43869763cd0a2790cb27c329
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-10-07 13:09:37 +0530

    feat(service): Add new component

---
+++ src/components/sobfa.js
@@ -0,0 +1,13 @@
+/**
+ * Sobfa utility functions
+ */
+
+export const sobfa = (params) => {
+  // Implementation
+  return {};
+};
+
+export const sobfaHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
