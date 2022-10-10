2e331be79769d6e6fb48a1a23c6fc2717454d0cd
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-10-10 11:41:53 +0530

    small refactor

--- public/component.jsx
+++ public/component.jsx
@@ -1,24 +1,13 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Component utility functions
+ */
 
-router.get('/component', async (req, res) => {
-  try {
-    // Fetch data logic
-    const data = await fetchComponentData();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+export const component = (config) => {
+  // Implementation
+  return {};
+};
 
-router.post('/component', async (req, res) => {
-  try {
-    const result = await createComponent(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-module.exports = router;
-// Updated: 2025-11-07T08:34:38.202Z
+export const componentHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
