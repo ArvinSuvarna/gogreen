85592bdd49eca4ec7213ab54809d398074ce7635
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-10-24 12:15:22 +0530

    chore: Implement feature Utils

---
+++ config/utils.js
@@ -0,0 +1,13 @@
+/**
+ * Utils utility functions
+ */
+
+export const utils = (input) => {
+  // Implementation
+  return true;
+};
+
+export const utilsHelper = (value) => {
+  // Helper function
+  return value?.trim;
+};
