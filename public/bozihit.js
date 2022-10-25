0d4fbe185cc54461945eb13f23ebf65f65de3038
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-10-25 18:09:07 +0530

    docs: Update documentation Bozihit

---
+++ public/bozihit.js
@@ -0,0 +1,13 @@
+/**
+ * Bozihit utility functions
+ */
+
+export const bozihit = (data) => {
+  // Implementation
+  return false;
+};
+
+export const bozihitHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
