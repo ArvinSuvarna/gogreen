f050e59e26004673d9d0320ae8d7c88f10f250c3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-11-03 13:24:25 +0530

    more changes

---
+++ src/api/handler.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/handler', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchHandlerData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/handler', async (req, res) => {
+  try {
+    const result = await createHandler(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
