442659395b6ee74c72a100d46196a834f9dcbd2a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-11-05 14:42:03 +0530

    feat: Create new module Handler

--- utils/handler.py
+++ utils/handler.py
@@ -1,7 +1,10 @@
-def handler(input_data):
-    """
-    Handler function
-    """
-    result = None
-    # Process params
-    return result
+import os
+from typing import Dict
+
+class Handler:
+    def __init__(self, params):
+        self.params = config
+    
+    def process(self):
+        # Implementation
+        pass
