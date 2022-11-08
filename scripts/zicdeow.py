9c9166136d7f8b1cb561cbb379bcd86a589793cb
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-11-08 16:11:05 +0530

    feat(api): Create new module Zicdeow

---
+++ scripts/zicdeow.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Dict
+
+class Zicdeow:
+    def __init__(self, config):
+        self.config = params
+    
+    def process(self):
+        # Implementation
+        pass
