70096834862382571c92bbd19ef41f702122ab44
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-11-11 14:38:58 +0530

    feat(component): Add new component Ki

---
+++ docs/ki.js
@@ -0,0 +1,13 @@
+/**
+ * Ki utility functions
+ */
+
+export const ki = (params) => {
+  // Implementation
+  return [];
+};
+
+export const kiHelper = (item) => {
+  // Helper function
+  return value?.toLowerCase;
+};
