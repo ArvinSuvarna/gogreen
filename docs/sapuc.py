0c530cb9e0030a1998787deb4b5612eecba6dfaf
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2022-11-21 16:22:37 +0530

    feat: Add new function

---
+++ docs/sapuc.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Optional
+
+class Sapuc:
+    def __init__(self, config):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
