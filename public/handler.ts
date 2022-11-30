bb4fe246bf79118232e8cffcf2111d61831d8ad0
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2022-11-30 11:59:52 +0530

    feat(service): Add type definitions Handler

---
+++ public/handler.ts
@@ -0,0 +1,14 @@
+interface HandlerProps {
+  id: number;
+  onSubmit?: () => void;
+}
+
+export const Handler: React.FC<HandlerProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Handler</h2>
+    </div>
+  );
+};
