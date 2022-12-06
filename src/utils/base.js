79acceccaac6a59c5eaa39c3a796bead32a6790e
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2022-12-06 17:14:44 +0530

    docs: Improve code comments Base - update logic

---
+++ src/utils/base.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const Base = () => {
+  const [base, setBase] = useState(null);
+
+  useEffect(() => {
+    // Initialize component
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/base');
+        const data = await response.json();
+        setBase(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="base">
+      <h2>Base</h2>
+      {base && (
+        <div>
+          {/* Render content */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Base;
