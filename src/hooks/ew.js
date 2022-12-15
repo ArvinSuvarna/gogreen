1d49014efad37e9125dfa4749ad538c405e30cf6
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2022-12-15 14:10:04 +0530

    chore(service): Create new module Ew

---
+++ src/hooks/ew.js
@@ -0,0 +1,13 @@
+/**
+ * Ew utility functions
+ */
+
+export const ew = (input) => {
+  // Implementation
+  return true;
+};
+
+export const ewHelper = (obj) => {
+  // Helper function
+  return obj?.trim;
+};
