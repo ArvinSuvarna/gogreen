ea01c9d0e17fda086f19a87b2c84c9f09e3129c4
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2022-12-16 14:11:36 +0530

    feat: Add new function Core - enhance functionality

---
+++ scripts/core.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Optional
+
+class Core:
+    def __init__(self, params):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
