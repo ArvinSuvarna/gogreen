54a542c969d9a7d0e947bedaae9d0990080509e0
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-01-10 15:19:03 +0530

    chore(api): Implement feature Epzu

---
+++ utils/epzu.js
@@ -0,0 +1,13 @@
+/**
+ * Epzu utility functions
+ */
+
+export const epzu = (data) => {
+  // Implementation
+  return null;
+};
+
+export const epzuHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
