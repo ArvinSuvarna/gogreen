ad1818ec256ced93779fdfc7f00c2cf6d9265489
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-01-17 18:43:50 +0530

    feat(service): Add utility function Sifo

---
+++ docs/sifo.js
@@ -0,0 +1,13 @@
+/**
+ * Sifo utility functions
+ */
+
+export const sifo = (data) => {
+  // Implementation
+  return true;
+};
+
+export const sifoHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
