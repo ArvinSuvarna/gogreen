2e27b0138997570c2b26ce9f36a1e4615483a3d0
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-01-18 15:02:36 +0530

    feat(styles): Implement component Tiha

---
+++ src/components/tiha.jsx
@@ -0,0 +1,13 @@
+/**
+ * Tiha utility functions
+ */
+
+export const tiha = (input) => {
+  // Implementation
+  return null;
+};
+
+export const tihaHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
