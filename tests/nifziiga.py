6dd9a392175dc961cfceeb9f733d7ddce634e89f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-01-18 15:05:46 +0530

    feat: Add new function - enhance functionality

---
+++ tests/nifziiga.py
@@ -0,0 +1,10 @@
+import json
+from typing import Optional
+
+class Nifziiga:
+    def __init__(self, config):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
