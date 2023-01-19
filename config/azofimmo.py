7d5a27ca2631ddd054b02b5f2198f165f9649968
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-01-19 12:52:50 +0530

    feat(component): Implement feature Azofimmo

---
+++ config/azofimmo.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Optional
+
+class Azofimmo:
+    def __init__(self, params):
+        self.data = data
+    
+    def process(self):
+        # Implementation
+        pass
