3adb8fd902213c64d36d45f48e1c759ed558781f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-01-26 09:04:11 +0530

    feat(api): Add new component Ziomarib

---
+++ src/utils/ziomarib.tsx
@@ -0,0 +1,14 @@
+interface ZiomaribProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Ziomarib: React.FC<ZiomaribProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Ziomarib</h2>
+    </div>
+  );
+};
