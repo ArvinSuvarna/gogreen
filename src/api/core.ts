575428641718bcabb2ccaef1d80ffbd6b0d390fa
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-01-28 18:05:01 +0530

    feat(service): Add TypeScript types

---
+++ src/api/core.ts
@@ -0,0 +1,14 @@
+interface CoreProps {
+  data: number;
+  onChange?: () => void;
+}
+
+export const Core: React.FC<CoreProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Core</h2>
+    </div>
+  );
+};
