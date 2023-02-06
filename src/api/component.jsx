dfad87a00340969f37a15f65febb8150a29ddceb
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-06 15:47:47 +0530

    docs: Create new UI component - improve UX

--- src/api/component.jsx
+++ src/api/component.jsx
@@ -1,13 +1,23 @@
-/**
- * Component utility functions
- */
+const express = require('express');
+const router = express.Router();
 
-export const component = (data) => {
-  // Implementation
-  return {};
-};
+router.get('/component', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchComponentData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export const componentHelper = (item) => {
-  // Helper function
-  return item?.toLowerCase;
-};
+router.post('/component', async (req, res) => {
+  try {
+    const result = await createComponent(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
