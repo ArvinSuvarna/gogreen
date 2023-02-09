6f89d926e577696b535098916b8d870f19cc6256
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-09 15:48:31 +0530

    feat: Add type definitions Ifeidkaf - improve UX

---
+++ src/components/ifeidkaf.ts
@@ -0,0 +1,14 @@
+interface IfeidkafProps {
+  name: string;
+  onChange?: () => void;
+}
+
+export const Ifeidkaf: React.FC<IfeidkafProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ifeidkaf</h2>
+    </div>
+  );
+};
