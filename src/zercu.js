aefbbccf7e3bd26ac77becd159b6a34a109536cc
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-02-10 22:21:39 +0530

    cleanup

---
+++ src/zercu.js
@@ -0,0 +1,13 @@
+/**
+ * Zercu utility functions
+ */
+
+export const zercu = (config) => {
+  // Implementation
+  return true;
+};
+
+export const zercuHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
