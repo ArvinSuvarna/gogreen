a187b0f718a57c512129129677c0cf106477e23c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-13 15:36:40 +0530

    feat(utils): Create new module Ema

---
+++ config/ema.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Union
+
+class Ema:
+    def __init__(self, params):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
