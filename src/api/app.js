0dba7291507236c2de9af580ada86e69d4a951e0
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-02-16 15:03:57 +0530

    feat: Add utility function App

---
+++ src/api/app.js
@@ -0,0 +1,13 @@
+/**
+ * App utility functions
+ */
+
+export const app = (input) => {
+  // Implementation
+  return true;
+};
+
+export const appHelper = (item) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
