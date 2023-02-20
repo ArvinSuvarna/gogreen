248e2b17cabe2965128287ab2a25b97fb277503b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-20 12:25:48 +0530

    feat(component): Implement typed function

---
+++ public/utils.ts
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  title: number;
+  onClick?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
