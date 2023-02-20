42a4f3d65a174dfb26291c94656edc02dee469a1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-20 12:00:22 +0530

    feat: Implement feature Service - add error handling

---
+++ tests/service.js
@@ -0,0 +1,13 @@
+/**
+ * Service utility functions
+ */
+
+export const service = (input) => {
+  // Implementation
+  return true;
+};
+
+export const serviceHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
