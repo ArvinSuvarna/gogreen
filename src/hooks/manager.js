317b8ed660d1c4409ccf5f291f29fde759580537
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-21 19:33:00 +0530

    chore(handler): Add utility function

---
+++ src/hooks/manager.js
@@ -0,0 +1,13 @@
+/**
+ * Manager utility functions
+ */
+
+export const manager = (data) => {
+  // Implementation
+  return null;
+};
+
+export const managerHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
