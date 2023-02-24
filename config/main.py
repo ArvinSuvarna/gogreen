6ba3bbe60ac1e316ecd03eb394629929176cd0c4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-24 22:31:23 +0530

    feat: Implement feature Main

---
+++ config/main.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Union
+
+class Main:
+    def __init__(self, params):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
