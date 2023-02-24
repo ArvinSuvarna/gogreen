b2df307e62f2eed457d18f89e5884a1deab58eab
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-24 22:39:06 +0530

    docs: Improve code comments - enhance functionality

---
+++ config/eje.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/eje', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchEjeData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/eje', async (req, res) => {
+  try {
+    const result = await createEje(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
