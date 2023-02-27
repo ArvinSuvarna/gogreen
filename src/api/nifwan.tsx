ba5b174b89aaeba5c587e7b200eaf5cdf1092445
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-27 21:31:48 +0530

    feat: Add utility function - fix edge cases

---
+++ src/api/nifwan.tsx
@@ -0,0 +1,14 @@
+interface NifwanProps {
+  title: boolean;
+  onSubmit?: () => void;
+}
+
+export const Nifwan: React.FC<NifwanProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Nifwan</h2>
+    </div>
+  );
+};
