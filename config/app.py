5535deb03502fbb7a2e5e0880d5fa88da68dd5f6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-02-28 19:36:06 +0530

    quick fix

--- config/app.py
+++ config/app.py
@@ -1,9 +1,9 @@
 import os
-from typing import List
+from typing import Dict
 
 class App:
     def __init__(self, params):
-        self.config = data
+        self.config = params
     
     def process(self):
         # Implementation
