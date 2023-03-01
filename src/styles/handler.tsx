1cbdeec20805d9954b141903ae365eb9152b9e89
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-01 20:01:49 +0530

    feat(hooks): Add utility function Handler

---
+++ src/styles/handler.tsx
@@ -0,0 +1,14 @@
+interface HandlerProps {
+  data: string;
+  onSubmit?: () => void;
+}
+
+export const Handler: React.FC<HandlerProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Handler</h2>
+    </div>
+  );
+};
