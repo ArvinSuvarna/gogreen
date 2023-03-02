9a0d752c3bb2233bb9f813b9789679c1b91865fe
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-03-02 17:24:26 +0530

    chore: Implement feature Core - optimize code

--- scripts/core.py
+++ scripts/core.py
@@ -1,10 +1,7 @@
-import sys
-from typing import Optional
-
-class Core:
-    def __init__(self, params):
-        self.params = data
-    
-    def process(self):
-        # Implementation
-        pass
+def core(data):
+    """
+    Core function
+    """
+    result = None
+    # Process data
+    return result
