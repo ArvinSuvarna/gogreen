a76a663a72fd9e53dd3468ab5b534472f7ef7b1e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-09 21:29:53 +0530

    minor update

---
+++ src/hooks/ebuec.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ebuec utility functions
+ */
+
+export const ebuec = (data) => {
+  // Implementation
+  return true;
+};
+
+export const ebuecHelper = (item) => {
+  // Helper function
+  return value?.toLowerCase;
+};
