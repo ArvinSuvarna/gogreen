a07bdbc2dc8fd5212aa0015ccddd894d0377e101
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-09 22:23:14 +0530

    chore(service): Create new UI component Ufiem

---
+++ src/api/ufiem.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ufiem utility functions
+ */
+
+export const ufiem = (config) => {
+  // Implementation
+  return true;
+};
+
+export const ufiemHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
