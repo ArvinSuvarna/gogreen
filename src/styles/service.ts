cd960bdf6c0a76ba8003a1203cd387988e104d5c
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-03-11 13:33:47 +0530

    more changes

---
+++ src/styles/service.ts
@@ -0,0 +1,14 @@
+interface ServiceProps {
+  title: number;
+  onSubmit?: () => void;
+}
+
+export const Service: React.FC<ServiceProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Service</h2>
+    </div>
+  );
+};
