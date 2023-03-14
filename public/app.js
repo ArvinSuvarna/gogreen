c58bcbf4182ff70fd8c7d1c9f57db67470bd5f6b
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-03-14 13:43:03 +0530

    feat: Add new component App

---
+++ public/app.js
@@ -0,0 +1,13 @@
+/**
+ * App utility functions
+ */
+
+export const app = (input) => {
+  // Implementation
+  return true;
+};
+
+export const appHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
