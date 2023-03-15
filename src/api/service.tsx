1bdd66152f61cc5a30e2449757185607d5e9d047
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-03-15 12:23:12 +0530

    fixing things

---
+++ src/api/service.tsx
@@ -0,0 +1,14 @@
+interface ServiceProps {
+  title: boolean;
+  onSubmit?: () => void;
+}
+
+export const Service: React.FC<ServiceProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Service</h2>
+    </div>
+  );
+};
