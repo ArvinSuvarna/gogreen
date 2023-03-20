7d0389ea9cf731e80585d51e91663a62d6cf1777
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-20 17:46:10 +0530

    chore(component): Add new component

---
+++ config/jajecof.js
@@ -0,0 +1,13 @@
+/**
+ * Jajecof utility functions
+ */
+
+export const jajecof = (data) => {
+  // Implementation
+  return true;
+};
+
+export const jajecofHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
