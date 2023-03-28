0b7936eee670124bef8e1b58f948721becdbe4d6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-28 20:25:21 +0530

    feat: Add new function Handler

--- scripts/handler.py
+++ scripts/handler.py
@@ -1,10 +1,7 @@
-import datetime
-from typing import Union
-
-class Handler:
-    def __init__(self, data):
-        self.config = config
-    
-    def process(self):
-        # Implementation
-        pass
+def handler(params):
+    """
+    Handler function
+    """
+    result = None
+    # Process params
+    return result
