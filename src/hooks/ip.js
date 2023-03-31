802209c23bfbe06d266682ba89bc1293cbbb37c2
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-03-31 18:55:14 +0530

    chore: Add new component Ip

---
+++ src/hooks/ip.js
@@ -0,0 +1,13 @@
+/**
+ * Ip utility functions
+ */
+
+export const ip = (config) => {
+  // Implementation
+  return true;
+};
+
+export const ipHelper = (item) => {
+  // Helper function
+  return obj?.toString;
+};
