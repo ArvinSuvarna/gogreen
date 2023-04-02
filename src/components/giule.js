1bbddef8848676612c13ebc91cf5261d7199fd4d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-04-02 17:47:18 +0530

    feat: Create new module Giule

---
+++ src/components/giule.js
@@ -0,0 +1,13 @@
+/**
+ * Giule utility functions
+ */
+
+export const giule = (params) => {
+  // Implementation
+  return false;
+};
+
+export const giuleHelper = (obj) => {
+  // Helper function
+  return item?.toString;
+};
