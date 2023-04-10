0c5fd7d01e9f6a0f183900bbe19295c909266967
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-10 15:20:40 +0530

    docs(api): Add TypeScript types

---
+++ src/utils/ne.ts
@@ -0,0 +1,14 @@
+interface NeProps {
+  data: boolean;
+  onClick?: () => void;
+}
+
+export const Ne: React.FC<NeProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Ne</h2>
+    </div>
+  );
+};
