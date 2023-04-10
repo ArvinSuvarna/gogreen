b09e90c8d567dfd38261ca5416aa927e951876f1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-04-10 17:16:52 +0530

    feat: Add new function Config - update logic

--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
-def config(data):
-    """
-    Config function
-    """
-    result = None
-    # Process params
-    return result
+import sys
+from typing import Dict
+
+class Config:
+    def __init__(self, config):
+        self.data = data
+    
+    def process(self):
+        # Implementation
+        pass
