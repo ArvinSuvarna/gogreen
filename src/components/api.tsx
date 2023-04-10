bfee04b646772f9c25fe63e5c9ca3607c3013a52
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-10 22:49:18 +0530

    chore: Create new module Api

---
+++ src/components/api.tsx
@@ -0,0 +1,14 @@
+interface ApiProps {
+  data: string;
+  onSubmit?: () => void;
+}
+
+export const Api: React.FC<ApiProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Api</h2>
+    </div>
+  );
+};
