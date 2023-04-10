f8ab8334d90c47ef19f57225741114d66f089667
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-10 12:20:20 +0530

    feat: Add utility function Index

---
+++ src/hooks/index.tsx
@@ -0,0 +1,14 @@
+interface IndexProps {
+  title: number;
+  onChange?: () => void;
+}
+
+export const Index: React.FC<IndexProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Index</h2>
+    </div>
+  );
+};
