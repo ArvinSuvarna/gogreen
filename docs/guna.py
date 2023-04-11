2878d7e17004fe3f6a9df3d79cdfe0700f23c6ee
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-11 11:27:56 +0530

    feat(api): Create new module Guna

---
+++ docs/guna.py
@@ -0,0 +1,10 @@
+import json
+from typing import Union
+
+class Guna:
+    def __init__(self, config):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
