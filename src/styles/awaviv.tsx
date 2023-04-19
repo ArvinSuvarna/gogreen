2f95bb2283820fef81d3aeffd0679669b578f3b6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-04-19 13:01:58 +0530

    feat: Implement feature Awaviv - fix edge cases

---
+++ src/styles/awaviv.tsx
@@ -0,0 +1,14 @@
+interface AwavivProps {
+  id: string;
+  onClick?: () => void;
+}
+
+export const Awaviv: React.FC<AwavivProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Awaviv</h2>
+    </div>
+  );
+};
