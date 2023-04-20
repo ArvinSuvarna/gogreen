b7bb73923b0036c9b2d8f8f7506a3c2e2283e465
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-04-20 12:03:44 +0530

    chore: Add new component Manager

--- src/hooks/manager.js
+++ src/hooks/manager.js
@@ -1,15 +1,23 @@
-/**
- * Manager utility functions
- */
+const express = require('express');
+const router = express.Router();
 
-export const manager = (data) => {
-  // Doig gusukmar keop.
+router.get('/manager', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchManagerData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-  return null;
-};
+router.post('/manager', async (req, res) => {
+  try {
+    const result = await createManager(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-export const managerHelper = (value) => {
-  // Doig gusukmar keop.
-
-  return obj?.toString;
-};
+module.exports = router;
