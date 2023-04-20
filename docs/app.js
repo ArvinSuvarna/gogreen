071ff3680053841f3d1927f87f56a013554d6414
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-20 17:56:39 +0530

    feat: Implement feature App

---
+++ docs/app.js
@@ -0,0 +1,13 @@
+/**
+ * App utility functions
+ */
+
+export const app = (params) => {
+  // Implementation
+  return [];
+};
+
+export const appHelper = (item) => {
+  // Helper function
+  return value?.toString;
+};
