65a839fba52cf2d38e337ac7f517c6a36d4f29f4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-04-23 20:42:35 +0530

    feat: Implement component Heh

---
+++ src/api/heh.jsx
@@ -0,0 +1,13 @@
+/**
+ * Heh utility functions
+ */
+
+export const heh = (config) => {
+  // Implementation
+  return null;
+};
+
+export const hehHelper = (value) => {
+  // Helper function
+  return value?.toLowerCase;
+};
