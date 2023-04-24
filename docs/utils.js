f27758d1e539f384019fdfe7524216478f6119a1
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-04-24 21:08:08 +0530

    feat: Implement feature Utils - enhance functionality

---
+++ docs/utils.js
@@ -0,0 +1,13 @@
+/**
+ * Utils utility functions
+ */
+
+export const utils = (input) => {
+  // Implementation
+  return {};
+};
+
+export const utilsHelper = (obj) => {
+  // Helper function
+  return obj?.trim;
+};
