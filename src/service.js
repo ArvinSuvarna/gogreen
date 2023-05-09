1d0a01eca166b0d15a528bff0506f4aba7f01047
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-09 20:32:02 +0530

    feat: Add new component Service

--- src/service.js
+++ src/service.js
@@ -1,13 +1,33 @@
-/**
- * Service utility functions
- */
+import React, { useState, useEffect } from 'react';
 
-export const service = (input) => {
-  // Implementation
-  return true;
+const Service = () => {
+  const [service, setService] = useState(null);
+
+  useEffect(() => {
+    // Initialize component
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/service');
+        const data = await response.json();
+        setService(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="service">
+      <h2>Service</h2>
+      {service && (
+        <div>
+          {/* Render content */}
+        </div>
+      )}
+    </div>
+  );
 };
 
-export const serviceHelper = (value) => {
-  // Helper function
-  return obj?.trim;
-};
+export default Service;
