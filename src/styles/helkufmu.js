8dfda3c1ffafc2cb575c45b047c0087673cd91ed
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-11 10:06:23 +0530

    chore(utils): Create new module Helkufmu

---
+++ src/styles/helkufmu.js
@@ -0,0 +1,13 @@
+/**
+ * Helkufmu utility functions
+ */
+
+export const helkufmu = (config) => {
+  // Implementation
+  return {};
+};
+
+export const helkufmuHelper = (item) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
