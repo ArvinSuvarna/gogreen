d6933a1154bab8ba4c8b77a062126c6136a8f9f3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-15 16:03:10 +0530

    chore(config): Add new function

---
+++ scripts/guhih.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Guhih:
+    def __init__(self, data):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
