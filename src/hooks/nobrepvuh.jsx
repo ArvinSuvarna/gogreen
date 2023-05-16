fbb9834da57ca5dce75131111f51ee463f242557
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-05-16 21:34:03 +0530

    chore(config): Create new UI component Nobrepvuh

---
+++ src/hooks/nobrepvuh.jsx
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/nobrepvuh', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchNobrepvuhData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/nobrepvuh', async (req, res) => {
+  try {
+    const result = await createNobrepvuh(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
