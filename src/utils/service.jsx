e700c660dcd2ccc928b7a300873b8d1246e7eb13
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-19 14:55:35 +0530

    feat(handler): Implement component Service

--- src/utils/service.jsx
+++ src/utils/service.jsx
@@ -1,23 +1,13 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Service utility functions
+ */
 
-router.get('/service', async (req, res) => {
-  try {
-    // Fetch data logic
-    const data = await fetchServiceData();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+export const service = (config) => {
+  // Implementation
+  return {};
+};
 
-router.post('/service', async (req, res) => {
-  try {
-    const result = await createService(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-module.exports = router;
+export const serviceHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
