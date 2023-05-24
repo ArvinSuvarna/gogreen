96b39fd7da0a59a84185bd48ea67fe29e2321b9c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-24 15:49:33 +0530

    chore(component): Create new module Utils

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,10 +1,10 @@
 interface UtilsProps {
-  name: string;
-  onSubmit?: () => void;
+  name: number;
+  onChange?: () => void;
 }
 
 export const Utils: React.FC<UtilsProps> = ({
-  data
+  title
 }) => {
   return (
     <div>
