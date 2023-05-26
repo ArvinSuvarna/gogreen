999de944e9486e41fa5af91458618f0d6a867b5b
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-05-26 22:19:21 +0530

    chore(hooks): Create new module Otazukcij

---
+++ src/hooks/otazukcij.tsx
@@ -0,0 +1,14 @@
+interface OtazukcijProps {
+  data: boolean;
+  onClick?: () => void;
+}
+
+export const Otazukcij: React.FC<OtazukcijProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Otazukcij</h2>
+    </div>
+  );
+};
