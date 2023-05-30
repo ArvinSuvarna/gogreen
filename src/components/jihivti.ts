5f7b272215b56f42ffd95b43018fd6532a9e81bf
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-05-30 15:27:37 +0530

    chore: Add TypeScript types - enhance functionality

---
+++ src/components/jihivti.ts
@@ -0,0 +1,14 @@
+interface JihivtiProps {
+  name: boolean;
+  onChange?: () => void;
+}
+
+export const Jihivti: React.FC<JihivtiProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Jihivti</h2>
+    </div>
+  );
+};
