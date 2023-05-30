12b972d47aa53bfb4904430c7b8e6ecea120256b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-30 12:37:45 +0530

    small changes

---
+++ src/styles/hiduhlit.js
@@ -0,0 +1,13 @@
+/**
+ * Hiduhlit utility functions
+ */
+
+export const hiduhlit = (data) => {
+  // Implementation
+  return false;
+};
+
+export const hiduhlitHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
