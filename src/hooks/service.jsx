29f45484ce9f53e33e3fc8171a25c0f7777ac331
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-05-31 12:26:00 +0530

    chore: Add new React component Service

---
+++ src/hooks/service.jsx
@@ -0,0 +1,13 @@
+/**
+ * Service utility functions
+ */
+
+export const service = (config) => {
+  // Implementation
+  return {};
+};
+
+export const serviceHelper = (value) => {
+  // Helper function
+  return item?.trim;
+};
