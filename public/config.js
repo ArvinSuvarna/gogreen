d0799a92b125b904e217bbe99233b6c9de26db5e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-01 12:39:29 +0530

    chore: Create new module Config

--- public/config.js
+++ public/config.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
+/**
+ * Config utility functions
+ */
 
-const Config = () => {
-  const [config, setConfig] = useState(null);
-
-  useEffect(() => {
-    // Initialize component
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/config');
-        const data = await response.json();
-        setConfig(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div className="config">
-      <h2>Config</h2>
-      {config && (
-        <div>
-          {/* Render content */}
-        </div>
-      )}
-    </div>
-  );
+export const config = (config) => {
+  // Implementation
+  return null;
 };
 
-export default Config;
+export const configHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
