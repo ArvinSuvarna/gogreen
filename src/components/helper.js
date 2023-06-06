2adb67ec860b39dd2c0c00671dad9dee90f36f23
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-06 13:25:39 +0530

    feat(handler): Create new module

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,33 +1,23 @@
-import React, { useState, useEffect } from 'react';
+const express = require('express');
+const router = express.Router();
 
-const Helper = () => {
-  const [helper, setHelper] = useState(null);
+router.get('/helper', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchHelperData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-  useEffect(() => {
-    // Initialize component
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/helper');
-        const data = await response.json();
-        setHelper(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+router.post('/helper', async (req, res) => {
+  try {
+    const result = await createHelper(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className="helper">
-      <h2>Helper</h2>
-      {helper && (
-        <div>
-          {/* Render content */}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Helper;
+module.exports = router;
