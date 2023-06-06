4abb65f6c7c34bd68e3bd5904dbcb99cfb53afc1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-06 17:50:31 +0530

    feat(hooks): Implement typed function Utils

---
+++ src/components/utils.ts
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  id: string;
+  onChange?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
