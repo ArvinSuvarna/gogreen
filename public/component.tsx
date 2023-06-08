95b9557c0c5bdeaf19b219123d0f0f9863bad60d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-06-08 17:51:59 +0530

    feat(api): Add utility function Component

---
+++ public/component.tsx
@@ -0,0 +1,14 @@
+interface ComponentProps {
+  title: string;
+  onClick?: () => void;
+}
+
+export const Component: React.FC<ComponentProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Component</h2>
+    </div>
+  );
+};
