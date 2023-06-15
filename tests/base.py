335674d8a258569e13b765704037ef62823b60ba
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-15 15:34:09 +0530

    chore: Implement feature Base

--- tests/base.py
+++ tests/base.py
@@ -1,7 +1,10 @@
-def base(params):
-    """
-    Base function
-    """
-    result = {}
-    # Process params
-    return result
+import os
+from typing import List
+
+class Base:
+    def __init__(self, data):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
