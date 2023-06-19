b0f1ee23ea906cf0ecd7200a7761401009cd78c5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-19 17:43:10 +0530

    docs: Improve code comments Index - improve performance

---
+++ src/utils/index.tsx
@@ -0,0 +1,14 @@
+interface IndexProps {
+  title: number;
+  onSubmit?: () => void;
+}
+
+export const Index: React.FC<IndexProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Index</h2>
+    </div>
+  );
+};
