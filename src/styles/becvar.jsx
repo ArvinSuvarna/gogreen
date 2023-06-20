85175f73dd0430769a2cd772789605219ee1822d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-20 19:45:26 +0530

    chore(styles): Create new UI component Becvar

---
+++ src/styles/becvar.jsx
@@ -0,0 +1,13 @@
+/**
+ * Becvar utility functions
+ */
+
+export const becvar = (config) => {
+  // Implementation
+  return [];
+};
+
+export const becvarHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
