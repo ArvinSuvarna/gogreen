3c656d72f042979e7e86513444dadf7825b9a504
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-21 22:15:56 +0530

    feat: Add new component Api - add error handling

---
+++ src/api/api.tsx
@@ -0,0 +1,14 @@
+interface ApiProps {
+  data: number;
+  onClick?: () => void;
+}
+
+export const Api: React.FC<ApiProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Api</h2>
+    </div>
+  );
+};
