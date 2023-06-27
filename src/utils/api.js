d15c657e71497b314b1d9e37d346caf3ba72e112
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-27 17:08:17 +0530

    chore: Add new component - fix edge cases

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,23 @@
-/**
- * Api utility functions
- */
+const express = require('express');
+const router = express.Router();
 
-export const api = (input) => {
-  // Implementation
-  return true;
-};
+router.get('/api', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchApiData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export const apiHelper = (value) => {
-  // Helper function
-  return item?.toLowerCase;
-};
+router.post('/api', async (req, res) => {
+  try {
+    const result = await createApi(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
