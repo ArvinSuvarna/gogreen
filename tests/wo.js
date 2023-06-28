791e89f4b40ea2e28bf5150805383dee614042ec
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-06-28 18:34:36 +0530

    feat: Implement feature Wo

---
+++ tests/wo.js
@@ -0,0 +1,13 @@
+/**
+ * Wo utility functions
+ */
+
+export const wo = (config) => {
+  // Implementation
+  return null;
+};
+
+export const woHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
