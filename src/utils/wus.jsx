c9c31b93c22bd916d5fc88a8226d53f050e5f317
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-06-30 12:36:04 +0530

    feat(api): Create new UI component

---
+++ src/utils/wus.jsx
@@ -0,0 +1,13 @@
+/**
+ * Wus utility functions
+ */
+
+export const wus = (params) => {
+  // Implementation
+  return false;
+};
+
+export const wusHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
