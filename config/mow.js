bfc2239cf5436ef39edf035ae07cdeee202db630
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-07-04 21:46:27 +0530

    feat: Implement feature

---
+++ config/mow.js
@@ -0,0 +1,13 @@
+/**
+ * Mow utility functions
+ */
+
+export const mow = (data) => {
+  // Implementation
+  return false;
+};
+
+export const mowHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
