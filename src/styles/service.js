b4f95f8ac59439e9dd7aeaa25265b7547402eb9d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-07-06 14:49:43 +0530

    chore(utils): Add new component Service

---
+++ src/styles/service.js
@@ -0,0 +1,13 @@
+/**
+ * Service utility functions
+ */
+
+export const service = (input) => {
+  // Implementation
+  return {};
+};
+
+export const serviceHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
