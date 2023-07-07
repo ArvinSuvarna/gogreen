aee08dcd3d6fd108e39832cfe01ac1d20030f4b6
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-07-07 13:13:21 +0530

    docs: Add documentation Api

---
+++ docs/api.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Dict
+
+class Api:
+    def __init__(self, params):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
