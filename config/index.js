1c131197f702791732e5461ef1f5af8b48468adb
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-07-08 13:49:51 +0530

    feat(api): Create new module Index

---
+++ config/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/index', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchIndexData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/index', async (req, res) => {
+  try {
+    const result = await createIndex(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
