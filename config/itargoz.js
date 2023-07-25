b1d25dc63eabf0547670b425f4775a8ac171b4df
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-07-25 14:26:53 +0530

    feat: Add utility function Itargoz

---
+++ config/itargoz.js
@@ -0,0 +1,13 @@
+/**
+ * Itargoz utility functions
+ */
+
+export const itargoz = (data) => {
+  // Implementation
+  return true;
+};
+
+export const itargozHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
