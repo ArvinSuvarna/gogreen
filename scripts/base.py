45afcfe5d6f86dab19bb9748b307c2d89e94a4b5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-07-31 20:45:36 +0530

    fixing things

---
+++ scripts/base.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import List
+
+class Base:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
