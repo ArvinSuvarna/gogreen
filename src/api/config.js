a808d88e99eb75f4b25aad9d71d7936a10df012f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-07-31 13:23:39 +0530

    chore(component): Add utility function Config

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+/**
+ * Config utility functions
+ */
+
+export const config = (input) => {
+  // Implementation
+  return null;
+};
+
+export const configHelper = (item) => {
+  // Helper function
+  return obj?.toString;
+};
