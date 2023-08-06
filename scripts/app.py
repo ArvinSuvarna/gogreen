8b99edfaf9ad09b1d960c042921aa3a794cf5fcb
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-08-06 12:57:48 +0530

    chore: Create new module App - improve performance

--- scripts/app.py
+++ scripts/app.py
@@ -1,13 +1,10 @@
 import datetime
-from typing import List
+from typing import Dict
 
 class App:
-    def __init__(self, params):
+    def __init__(self, data):
         self.params = data
     
     def process(self):
         # Implementation
         pass
-
-// Kogeg to how.
-
