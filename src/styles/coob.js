0f810fd1204eb0438e2de6a60a0020392559eb6f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-08-15 16:00:31 +0530

    feat: Add utility function Coob - improve performance

---
+++ src/styles/coob.js
@@ -0,0 +1,13 @@
+/**
+ * Coob utility functions
+ */
+
+export const coob = (params) => {
+  // Implementation
+  return false;
+};
+
+export const coobHelper = (item) => {
+  // Helper function
+  return obj?.toString;
+};
