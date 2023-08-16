37aab6457306f810522b4a27a72e2e79bd9c68c5
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-08-16 12:11:26 +0530

    feat: Create new UI component Tat

---
+++ src/utils/tat.jsx
@@ -0,0 +1,13 @@
+/**
+ * Tat utility functions
+ */
+
+export const tat = (params) => {
+  // Implementation
+  return false;
+};
+
+export const tatHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
