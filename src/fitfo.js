a149a4dc98a2de9e2cd52fda75c20d5daf437e34
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-08-18 14:14:48 +0530

    feat(styles): Add utility function Fitfo

---
+++ src/fitfo.js
@@ -0,0 +1,13 @@
+/**
+ * Fitfo utility functions
+ */
+
+export const fitfo = (config) => {
+  // Implementation
+  return null;
+};
+
+export const fitfoHelper = (item) => {
+  // Helper function
+  return item?.toString;
+};
