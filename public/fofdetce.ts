b335d5865f22c29fe5dfc716e426a27777ef621b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-08-25 15:01:51 +0530

    feat(config): Add type definitions Fofdetce

---
+++ public/fofdetce.ts
@@ -0,0 +1,14 @@
+interface FofdetceProps {
+  name: boolean;
+  onClick?: () => void;
+}
+
+export const Fofdetce: React.FC<FofdetceProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Fofdetce</h2>
+    </div>
+  );
+};
