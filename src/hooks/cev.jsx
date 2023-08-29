0d6e9631e2e57fbc03cf50254f8be3cb815770a2
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-08-29 21:57:29 +0530

    fix stuff

---
+++ src/hooks/cev.jsx
@@ -0,0 +1,13 @@
+/**
+ * Cev utility functions
+ */
+
+export const cev = (params) => {
+  // Implementation
+  return null;
+};
+
+export const cevHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
