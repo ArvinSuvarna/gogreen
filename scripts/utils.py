a724326f880243554f0ef5738a09029db30beca2
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-04 11:00:29 +0530

    feat: Create new module Utils

---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+import os
+from typing import Dict
+
+class Utils:
+    def __init__(self, params):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
