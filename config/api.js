7d2efe6f3a5686b45e16e6937b2fc2b3369a8a00
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-05 14:16:05 +0530

    feat(config): Implement feature Api

---
+++ config/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchApiData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/api', async (req, res) => {
+  try {
+    const result = await createApi(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
