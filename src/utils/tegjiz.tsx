3cf40ea068764ac9cfee5d56fad11b84e3c7edb2
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-06 13:23:01 +0530

    feat: Add new component

---
+++ src/utils/tegjiz.tsx
@@ -0,0 +1,14 @@
+interface TegjizProps {
+  data: boolean;
+  onSubmit?: () => void;
+}
+
+export const Tegjiz: React.FC<TegjizProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Tegjiz</h2>
+    </div>
+  );
+};
