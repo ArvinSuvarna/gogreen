cfc535706508b2808a78208b5c06f3b9448ef83f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-06 09:16:42 +0530

    feat: Add type definitions App - fix edge cases

---
+++ src/api/app.ts
@@ -0,0 +1,14 @@
+interface AppProps {
+  id: number;
+  onSubmit?: () => void;
+}
+
+export const App: React.FC<AppProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>App</h2>
+    </div>
+  );
+};
