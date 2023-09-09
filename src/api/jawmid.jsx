af2b364743c4e90240b975033cd3eb04e0a169f8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-09-09 14:25:21 +0530

    minor update

---
+++ src/api/jawmid.jsx
@@ -0,0 +1,13 @@
+/**
+ * Jawmid utility functions
+ */
+
+export const jawmid = (data) => {
+  // Implementation
+  return false;
+};
+
+export const jawmidHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
