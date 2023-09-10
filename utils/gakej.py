5456dc71a7a1f02a507583d190b1d5e57403ceeb
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-10 16:56:26 +0530

    small refactor

---
+++ utils/gakej.py
@@ -0,0 +1,10 @@
+import json
+from typing import Optional
+
+class Gakej:
+    def __init__(self, data):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
