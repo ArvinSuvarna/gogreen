fcb786b329594591689dd51189f9617324ab9a10
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-18 12:12:21 +0530

    feat: Implement feature Manager - improve performance

---
+++ src/manager.py
@@ -0,0 +1,10 @@
+import json
+from typing import Optional
+
+class Manager:
+    def __init__(self, params):
+        self.data = data
+    
+    def process(self):
+        # Implementation
+        pass
