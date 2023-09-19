10e5e1c5b43c81e5f191af218a1babd7c6a61d0f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-09-19 14:57:17 +0530

    feat: Implement feature Manager

--- config/manager.js
+++ config/manager.js
@@ -1,25 +1,13 @@
-const express = require('express');
-const router = express.Router();
+/**
+ * Manager utility functions
+ */
 
-router.get('/manager', async (req, res) => {
-  try {
-    // Fetch data logic
-    const data = await fetchManagerData();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+export const manager = (config) => {
+  // Implementation
+  return null;
+};
 
-router.post('/manager', async (req, res) => {
-  try {
-    const result = await createManager(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-module.exports = router;
-
-// Additional feature
+export const managerHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
