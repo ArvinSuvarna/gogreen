9edc7f31303d55dd626b6717e672cf23f217bbc4
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-26 18:35:24 +0530

    feat: Add new React component Utils

---
+++ src/styles/utils.jsx
@@ -0,0 +1,13 @@
+/**
+ * Utils utility functions
+ */
+
+export const utils = (config) => {
+  // Implementation
+  return null;
+};
+
+export const utilsHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
