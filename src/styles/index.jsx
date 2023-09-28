23c09e18a8243daf689af3b158784b53ee191fa4
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-09-28 11:14:03 +0530

    chore: Implement component Index - fix edge cases

---
+++ src/styles/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * Index utility functions
+ */
+
+export const index = (data) => {
+  // Implementation
+  return false;
+};
+
+export const indexHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
