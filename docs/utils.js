9360f6b95000178b60975f3aa7f0ae86628111f8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-09-28 12:59:08 +0530

    docs: Update documentation Utils - add error handling

--- docs/utils.js
+++ docs/utils.js
@@ -1,13 +1,33 @@
-/**
- * Utils utility functions
- */
+import React, { useState, useEffect } from 'react';
 
-export const utils = (input) => {
-  // Implementation
-  return {};
+const Utils = () => {
+  const [utils, setUtils] = useState(null);
+
+  useEffect(() => {
+    // Initialize component
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/utils');
+        const data = await response.json();
+        setUtils(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="utils">
+      <h2>Utils</h2>
+      {utils && (
+        <div>
+          {/* Render content */}
+        </div>
+      )}
+    </div>
+  );
 };
 
-export const utilsHelper = (obj) => {
-  // Helper function
-  return obj?.trim;
-};
+export default Utils;
