804fd055c1963252dd3994527f489f2d02ee63a9
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-05 18:13:01 +0530

    docs: Update docstrings Po

---
+++ utils/po.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Optional
+
+class Po:
+    def __init__(self, params):
+        self.config = params
+    
+    def process(self):
+        # Implementation
+        pass
