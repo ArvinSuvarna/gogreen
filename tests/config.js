68ca720d3f7fd3759e4bd2003f5ef4cf7ee46b87
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-07 14:08:59 +0530

    tweaks

---
+++ tests/config.js
@@ -0,0 +1,13 @@
+/**
+ * Config utility functions
+ */
+
+export const config = (config) => {
+  // Implementation
+  return {};
+};
+
+export const configHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
