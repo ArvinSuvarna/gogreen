d86d854b6d8e5e1db2456b3d18fb58305d60980d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-17 16:16:27 +0530

    feat: Add utility function Index

---
+++ utils/index.js
@@ -0,0 +1,13 @@
+/**
+ * Index utility functions
+ */
+
+export const index = (config) => {
+  // Implementation
+  return [];
+};
+
+export const indexHelper = (value) => {
+  // Helper function
+  return item?.trim;
+};
