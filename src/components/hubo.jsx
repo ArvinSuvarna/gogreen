264afc1e92fb94b2a764ba39ae5d9c200d20ab2f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-19 12:49:19 +0530

    docs: Add new React component Hubo

---
+++ src/components/hubo.jsx
@@ -0,0 +1,13 @@
+/**
+ * Hubo utility functions
+ */
+
+export const hubo = (data) => {
+  // Implementation
+  return [];
+};
+
+export const huboHelper = (obj) => {
+  // Helper function
+  return value?.toString;
+};
