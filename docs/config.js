bfff9bb917dee407af53d616c3c31d08961ac68d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-23 16:05:48 +0530

    feat(component): Create new module Config

---
+++ docs/config.js
@@ -0,0 +1,13 @@
+/**
+ * Config utility functions
+ */
+
+export const config = (input) => {
+  // Implementation
+  return [];
+};
+
+export const configHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
