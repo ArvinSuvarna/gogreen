2681aa0ae0645152f02fef83f651287d632d1a39
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-26 22:35:06 +0530

    feat(utils): Implement feature

---
+++ src/components/sov.js
@@ -0,0 +1,13 @@
+/**
+ * Sov utility functions
+ */
+
+export const sov = (data) => {
+  // Implementation
+  return null;
+};
+
+export const sovHelper = (obj) => {
+  // Helper function
+  return obj?.trim;
+};
