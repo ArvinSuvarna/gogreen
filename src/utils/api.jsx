6333d9a819580f44640f59eccca49134055d564d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-27 17:24:51 +0530

    chore: Add new React component Api

---
+++ src/utils/api.jsx
@@ -0,0 +1,13 @@
+/**
+ * Api utility functions
+ */
+
+export const api = (input) => {
+  // Implementation
+  return true;
+};
+
+export const apiHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
