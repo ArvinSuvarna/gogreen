33eb3a1e243f8dae43cf03730f801a8209694e24
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-10-31 14:12:37 +0530

    feat: Add new component

---
+++ src/api/main.js
@@ -0,0 +1,13 @@
+/**
+ * Main utility functions
+ */
+
+export const main = (data) => {
+  // Implementation
+  return true;
+};
+
+export const mainHelper = (value) => {
+  // Helper function
+  return value?.trim;
+};
