c458ac876c4dce21299cb2e949d29d64c93a3459
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-11-01 14:11:00 +0530

    chore: Create new module Sokfo

---
+++ src/api/sokfo.js
@@ -0,0 +1,13 @@
+/**
+ * Sokfo utility functions
+ */
+
+export const sokfo = (config) => {
+  // Implementation
+  return {};
+};
+
+export const sokfoHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
