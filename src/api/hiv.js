e73725205329e4649ead48b6e12ccf58e22b6bf5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-11-13 14:01:25 +0530

    docs: Update documentation Hiv - fix edge cases

---
+++ src/api/hiv.js
@@ -0,0 +1,13 @@
+/**
+ * Hiv utility functions
+ */
+
+export const hiv = (config) => {
+  // Implementation
+  return null;
+};
+
+export const hivHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
