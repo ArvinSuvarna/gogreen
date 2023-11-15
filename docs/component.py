78c9306d427aa6368a627a8cbc5e0659c9e54502
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-11-15 15:18:04 +0530

    docs: Update docstrings Component

---
+++ docs/component.py
@@ -0,0 +1,10 @@
+import os
+from typing import List
+
+class Component:
+    def __init__(self, params):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
