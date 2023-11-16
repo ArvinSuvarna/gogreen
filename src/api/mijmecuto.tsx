e1f0cde4b6ae8af72789789229578ad14c59a605
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-11-16 11:58:23 +0530

    feat(utils): Add utility function

---
+++ src/api/mijmecuto.tsx
@@ -0,0 +1,14 @@
+interface MijmecutoProps {
+  id: number;
+  onSubmit?: () => void;
+}
+
+export const Mijmecuto: React.FC<MijmecutoProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Mijmecuto</h2>
+    </div>
+  );
+};
