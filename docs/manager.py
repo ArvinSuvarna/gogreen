675d4692efa64681627cebcf4c7364cb11d93bb6
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-11-20 09:53:44 +0530

    chore(hooks): Create new module Manager

---
+++ docs/manager.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Dict
+
+class Manager:
+    def __init__(self, config):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
