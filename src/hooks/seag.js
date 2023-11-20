8845f1a6af0cd78abdd8c000b5badecc65d89073
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-11-20 09:48:09 +0530

    feat(service): Add new component Seag

---
+++ src/hooks/seag.js
@@ -0,0 +1,13 @@
+/**
+ * Seag utility functions
+ */
+
+export const seag = (data) => {
+  // Implementation
+  return null;
+};
+
+export const seagHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
