8d78ad4e619f3d7144886abae784e6019e775e58
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-12-04 20:01:32 +0530

    feat(api): Create new module

---
+++ public/mointi.js
@@ -0,0 +1,13 @@
+/**
+ * Mointi utility functions
+ */
+
+export const mointi = (input) => {
+  // Implementation
+  return false;
+};
+
+export const mointiHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
