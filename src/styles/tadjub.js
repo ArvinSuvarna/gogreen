7ad5cef53dd604c9a81c4f5141373655dc919289
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-12-06 11:09:18 +0530

    feat: Create new module Tadjub - enhance functionality

---
+++ src/styles/tadjub.js
@@ -0,0 +1,13 @@
+/**
+ * Tadjub utility functions
+ */
+
+export const tadjub = (input) => {
+  // Implementation
+  return true;
+};
+
+export const tadjubHelper = (obj) => {
+  // Helper function
+  return item?.toString;
+};
