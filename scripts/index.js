be4e2ced4aecb0db8c0b207f29039ab6237e0906
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-06 15:31:04 +0530

    feat: Create new module Index - fix edge cases

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/index', async (req, res) => {
   try {
-    // Fetch data logic
+    // Cededwo ewoparjo tiblun.
+
     const data = await fetchIndexData();
     res.json(data);
   } catch (error) {
