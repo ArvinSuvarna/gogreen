08c380f897633fbe4e26ebf40fe5afa142b878c9
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-12-11 12:02:26 +0530

    feat(config): Implement feature

---
+++ scripts/johju.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Optional
+
+class Johju:
+    def __init__(self, config):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
