94ab2bf6f5bc746c1bc231625bf57faf2ebdbc32
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-11 20:14:38 +0530

    feat(handler): Add new React component Api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,33 +1,23 @@
-import React, { useState, useEffect } from 'react';
+const express = require('express');
+const router = express.Router();
 
-const Api = () => {
-  const [api, setApi] = useState(null);
+router.get('/api', async (req, res) => {
+  try {
+    // Fetch data logic
+    const data = await fetchApiData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-  useEffect(() => {
-    // Initialize component
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/api');
-        const data = await response.json();
-        setApi(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+router.post('/api', async (req, res) => {
+  try {
+    const result = await createApi(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className="api">
-      <h2>Api</h2>
-      {api && (
-        <div>
-          {/* Render content */}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Api;
+module.exports = router;
