21baa62e2221a1e7817a6fa9449f2839b22e2e0e
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-12 18:32:50 +0530

    chore: Add type definitions Api

---
+++ src/styles/api.ts
@@ -0,0 +1,14 @@
+interface ApiProps {
+  data: string;
+  onSubmit?: () => void;
+}
+
+export const Api: React.FC<ApiProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Api</h2>
+    </div>
+  );
+};
