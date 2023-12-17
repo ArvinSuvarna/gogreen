64df32a497300b845a06ab9f435025266a36795f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-17 17:23:42 +0530

    feat: Implement feature Base - update logic

---
+++ config/base.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import List
+
+class Base:
+    def __init__(self, data):
+        self.data = data
+    
+    def process(self):
+        # Implementation
+        pass
