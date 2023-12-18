fc13ae6603124b19125c60aaf9e702992f292e46
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-18 17:32:58 +0530

    small changes

---
+++ tests/ruk.js
@@ -0,0 +1,13 @@
+/**
+ * Ruk utility functions
+ */
+
+export const ruk = (data) => {
+  // Implementation
+  return {};
+};
+
+export const rukHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
