6ea2c4f6f36347b301d4229086d6b16f00ceae07
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2023-12-20 14:36:31 +0530

    feat: Add new function Main

---
+++ docs/main.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Dict
+
+class Main:
+    def __init__(self, params):
+        self.params = config
+    
+    def process(self):
+        # Implementation
+        pass
