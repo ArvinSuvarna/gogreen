0c725679d7dde040a892489beebb5e17aca1962d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2023-12-20 14:58:51 +0530

    feat(styles): Create new module Amawo

---
+++ docs/amawo.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Amawo:
+    def __init__(self, config):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
