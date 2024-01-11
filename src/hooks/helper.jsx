7f53932bae5597564f0a6e01a540a01327698745
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-01-11 13:34:58 +0530

    feat(handler): Add new React component Helper

---
+++ src/hooks/helper.jsx
@@ -0,0 +1,13 @@
+/**
+ * Helper utility functions
+ */
+
+export const helper = (config) => {
+  // Implementation
+  return true;
+};
+
+export const helperHelper = (obj) => {
+  // Helper function
+  return item?.toString;
+};
