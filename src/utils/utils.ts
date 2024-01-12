c11b194226e2662ac92fe1dd896f78762a13167d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-01-12 22:47:49 +0530

    feat(service): Add type definitions Utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  id: boolean;
+  onChange?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
