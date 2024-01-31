7c534331e5ba6273ae4005b9a718dbe989f66d0c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-01-31 16:35:31 +0530

    feat(handler): Add type definitions Component

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,10 +1,10 @@
 interface ComponentProps {
-  id: boolean;
-  onClick?: () => void;
+  data: boolean;
+  onSubmit?: () => void;
 }
 
 export const Component: React.FC<ComponentProps> = ({
-  data
+  name
 }) => {
   return (
     <div>
