ff924481124d7640555fac1a36a91b233920e1ec
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-01 14:16:22 +0530

    updates and fixes

---
+++ src/hooks/service.tsx
@@ -0,0 +1,14 @@
+interface ServiceProps {
+  id: string;
+  onSubmit?: () => void;
+}
+
+export const Service: React.FC<ServiceProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Service</h2>
+    </div>
+  );
+};
