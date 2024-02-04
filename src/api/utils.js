01ef533c1d95885776f565fddb0648c9a43be0cd
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-02-04 14:11:13 +0530

    feat: Implement feature Utils

---
+++ src/api/utils.js
@@ -0,0 +1,13 @@
+/**
+ * Utils utility functions
+ */
+
+export const utils = (config) => {
+  // Implementation
+  return false;
+};
+
+export const utilsHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
