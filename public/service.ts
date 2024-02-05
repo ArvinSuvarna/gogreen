c1e3243306f2854dfad18392a877d82bd21d4f91
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-02-05 14:43:09 +0530

    feat: Add TypeScript types Service

---
+++ public/service.ts
@@ -0,0 +1,14 @@
+interface ServiceProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Service: React.FC<ServiceProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Service</h2>
+    </div>
+  );
+};
