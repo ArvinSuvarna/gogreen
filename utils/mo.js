66386738a91cfa1fade144b36c909993f1eec7c8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-06 14:53:17 +0530

    feat: Implement feature Mo - add error handling

---
+++ utils/mo.js
@@ -0,0 +1,13 @@
+/**
+ * Mo utility functions
+ */
+
+export const mo = (data) => {
+  // Implementation
+  return true;
+};
+
+export const moHelper = (item) => {
+  // Helper function
+  return item?.toString;
+};
