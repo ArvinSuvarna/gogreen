f30d766a4924aa41fe04158aa91cb80675340f46
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-12 21:05:52 +0530

    feat: Create new module

---
+++ src/api/ha.js
@@ -0,0 +1,13 @@
+/**
+ * Ha utility functions
+ */
+
+export const ha = (input) => {
+  // Implementation
+  return false;
+};
+
+export const haHelper = (obj) => {
+  // Helper function
+  return value?.trim;
+};
