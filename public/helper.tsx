95312491ca1d6b96ee9ea078b8ece3056ab0bd52
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-21 21:30:34 +0530

    chore(component): Add utility function Helper

--- public/helper.tsx
+++ public/helper.tsx
@@ -1,10 +1,10 @@
 interface HelperProps {
-  title: number;
-  onChange?: () => void;
+  data: number;
+  onClick?: () => void;
 }
 
 export const Helper: React.FC<HelperProps> = ({
-  title
+  data
 }) => {
   return (
     <div>
