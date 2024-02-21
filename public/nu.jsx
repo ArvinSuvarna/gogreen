8e0eacfa822f2900e9ec22c0cf606ab26341d9aa
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-21 18:57:30 +0530

    feat: Create new UI component Nu

---
+++ public/nu.jsx
@@ -0,0 +1,13 @@
+/**
+ * Nu utility functions
+ */
+
+export const nu = (data) => {
+  // Implementation
+  return false;
+};
+
+export const nuHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
