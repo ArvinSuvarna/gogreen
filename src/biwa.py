06ddc8d14c8c3a98b0c1a3f3c9c409ff71cbd870
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-02-22 09:02:52 +0530

    feat: Implement feature

---
+++ src/biwa.py
@@ -0,0 +1,10 @@
+import os
+from typing import Union
+
+class Biwa:
+    def __init__(self, config):
+        self.params = config
+    
+    def process(self):
+        # Implementation
+        pass
