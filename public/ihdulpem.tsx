04091a4331ed8c6a36ec114346c39211545dbf0b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-03-13 16:18:04 +0530

    feat(config): Add new component

---
+++ public/ihdulpem.tsx
@@ -0,0 +1,14 @@
+interface IhdulpemProps {
+  name: string;
+  onClick?: () => void;
+}
+
+export const Ihdulpem: React.FC<IhdulpemProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Ihdulpem</h2>
+    </div>
+  );
+};
