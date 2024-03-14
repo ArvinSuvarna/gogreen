c0a6bbbe64af6971205add49450bdc8486468531
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-03-14 17:11:43 +0530

    feat: Add TypeScript types Disigih

---
+++ public/disigih.ts
@@ -0,0 +1,14 @@
+interface DisigihProps {
+  id: string;
+  onChange?: () => void;
+}
+
+export const Disigih: React.FC<DisigihProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Disigih</h2>
+    </div>
+  );
+};
