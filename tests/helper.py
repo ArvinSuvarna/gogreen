3132ad6560af9d832cfef4bf5ffde8594b58b545
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-03-21 16:07:47 +0530

    feat: Add new function

---
+++ tests/helper.py
@@ -0,0 +1,10 @@
+import json
+from typing import List
+
+class Helper:
+    def __init__(self, config):
+        self.params = config
+    
+    def process(self):
+        # Implementation
+        pass
