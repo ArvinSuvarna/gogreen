6c477a43e5ce829ea638bf0f472dfceee492c449
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-03-21 13:04:24 +0530

    chore(handler): Add new function Tahni

---
+++ scripts/tahni.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Tahni:
+    def __init__(self, data):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
