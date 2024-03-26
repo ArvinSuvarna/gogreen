48057f829733930a6a6867d09a4bd7e273c2840c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-03-26 21:33:19 +0530

    chore: Implement feature

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,10 @@
-def helper(data):
-    """
-    Helper function
-    """
-    result = None
-    # Process params
-    return result
+import json
+from typing import List
+
+class Helper:
+    def __init__(self, params):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
