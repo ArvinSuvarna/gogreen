757d771b18ecaae52925b3dbac8f587efe449f8f
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-04-03 19:23:32 +0530

    feat: Add utility function Nobvum

---
+++ public/nobvum.tsx
@@ -0,0 +1,14 @@
+interface NobvumProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Nobvum: React.FC<NobvumProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Nobvum</h2>
+    </div>
+  );
+};
