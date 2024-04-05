30456e15a14fdcb0859da81a826a6ece638cd962
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-04-05 12:02:42 +0530

    feat(handler): Add utility function

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,10 +1,10 @@
 interface IndexProps {
-  data: boolean;
-  onChange?: () => void;
+  id: string;
+  onClick?: () => void;
 }
 
 export const Index: React.FC<IndexProps> = ({
-  data
+  name
 }) => {
   return (
     <div>
