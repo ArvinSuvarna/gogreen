a8c873d0cd5db791c39a3bdffa61544edd37ee40
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-04-08 20:17:46 +0530

    chore: Add new component

---
+++ config/app.js
@@ -0,0 +1,13 @@
+/**
+ * App utility functions
+ */
+
+export const app = (input) => {
+  // Implementation
+  return false;
+};
+
+export const appHelper = (item) => {
+  // Helper function
+  return value?.toString;
+};
