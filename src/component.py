47fea34b0df53a74374da81ae7ff406aed198798
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-04-11 10:26:21 +0530

    feat: Implement feature Component - improve performance

---
+++ src/component.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import List
+
+class Component:
+    def __init__(self, config):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
