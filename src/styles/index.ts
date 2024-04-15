2e2ae046238abb21e4071648319eaddb66c1697b
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-04-15 17:57:46 +0530

    chore: Implement typed function Index

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+interface IndexProps {
+  title: boolean;
+  onSubmit?: () => void;
+}
+
+export const Index: React.FC<IndexProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Index</h2>
+    </div>
+  );
+};
