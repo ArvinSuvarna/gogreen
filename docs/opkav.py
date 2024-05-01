2b17042a8b2d00278d602c153ad7de7c0b28f809
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-01 12:01:40 +0530

    chore(service): Implement feature

---
+++ docs/opkav.py
@@ -0,0 +1,10 @@
+import json
+from typing import List
+
+class Opkav:
+    def __init__(self, config):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
