006f77b4cfefee7d93457c6b8fb985e2ffe4581a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-08 15:54:36 +0530

    feat: Add utility function Jasefaz

---
+++ tests/jasefaz.js
@@ -0,0 +1,13 @@
+/**
+ * Jasefaz utility functions
+ */
+
+export const jasefaz = (params) => {
+  // Implementation
+  return false;
+};
+
+export const jasefazHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
