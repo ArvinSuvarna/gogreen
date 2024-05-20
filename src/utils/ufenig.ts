2ac1cee826d1fd4394e244a5787ea0115279e6f5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-20 21:10:25 +0530

    feat: Add TypeScript types Ufenig

---
+++ src/utils/ufenig.ts
@@ -0,0 +1,14 @@
+interface UfenigProps {
+  name: number;
+  onSubmit?: () => void;
+}
+
+export const Ufenig: React.FC<UfenigProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ufenig</h2>
+    </div>
+  );
+};
