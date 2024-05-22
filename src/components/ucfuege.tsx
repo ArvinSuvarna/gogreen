0ad02778e2246f7c5dfddaf82cd0bdb14b8118e3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-22 15:52:47 +0530

    updates and fixes

---
+++ src/components/ucfuege.tsx
@@ -0,0 +1,14 @@
+interface UcfuegeProps {
+  name: boolean;
+  onClick?: () => void;
+}
+
+export const Ucfuege: React.FC<UcfuegeProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ucfuege</h2>
+    </div>
+  );
+};
