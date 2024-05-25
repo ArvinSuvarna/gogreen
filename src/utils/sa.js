b11834e81e4cecf943808222613c75407a2a56a0
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-05-25 13:27:09 +0530

    small refactor

---
+++ src/utils/sa.js
@@ -0,0 +1,13 @@
+/**
+ * Sa utility functions
+ */
+
+export const sa = (params) => {
+  // Implementation
+  return null;
+};
+
+export const saHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
