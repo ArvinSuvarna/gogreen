1a67dfa726a494f4bccf2d4b5dbc1fa53ae7ec1a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-27 14:39:44 +0530

    feat: Create new module

---
+++ scripts/bik.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Union
+
+class Bik:
+    def __init__(self, params):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
