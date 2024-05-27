8fae69cca41b21f8f5db9755ecc7699f062182c4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-27 20:36:40 +0530

    feat: Implement feature Wijcoev

---
+++ src/wijcoev.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Wijcoev:
+    def __init__(self, data):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
