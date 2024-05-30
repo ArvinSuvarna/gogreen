16604c72b1af7154d80abcfa853f36eb4c6d9176
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-05-30 13:02:24 +0530

    chore: Create new module - add validation

---
+++ src/hooks/ozdito.tsx
@@ -0,0 +1,14 @@
+interface OzditoProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Ozdito: React.FC<OzditoProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ozdito</h2>
+    </div>
+  );
+};
