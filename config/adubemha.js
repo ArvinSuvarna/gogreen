cb67d4336880e6ef934aa95436b58b4ad7c97153
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-30 14:55:25 +0530

    feat(api): Implement feature Adubemha

---
+++ config/adubemha.js
@@ -0,0 +1,13 @@
+/**
+ * Adubemha utility functions
+ */
+
+export const adubemha = (config) => {
+  // Implementation
+  return {};
+};
+
+export const adubemhaHelper = (obj) => {
+  // Helper function
+  return value?.trim;
+};
