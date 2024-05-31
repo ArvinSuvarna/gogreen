0d49a55e33aa7cb64e0b3b9e6bfe66cb57326bae
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-05-31 16:22:10 +0530

    feat: Create new module - update logic

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,10 +1,10 @@
 interface BaseProps {
-  data: boolean;
-  onChange?: () => void;
+  id: number;
+  onClick?: () => void;
 }
 
 export const Base: React.FC<BaseProps> = ({
-  data
+  name
 }) => {
   return (
     <div>
