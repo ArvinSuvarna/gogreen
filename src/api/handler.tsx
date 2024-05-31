d7beb48ec2ae5ae835c38d60451874bb6f2a3ef3
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-05-31 10:45:57 +0530

    chore: Implement feature Handler

---
+++ src/api/handler.tsx
@@ -0,0 +1,14 @@
+interface HandlerProps {
+  title: number;
+  onChange?: () => void;
+}
+
+export const Handler: React.FC<HandlerProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Handler</h2>
+    </div>
+  );
+};
