5dfc494679c3b32efaf4d6167065e2d60104179e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-06-05 13:19:23 +0530

    minor update

---
+++ src/selufri.js
@@ -0,0 +1,13 @@
+/**
+ * Selufri utility functions
+ */
+
+export const selufri = (config) => {
+  // Implementation
+  return {};
+};
+
+export const selufriHelper = (value) => {
+  // Helper function
+  return value?.toLowerCase;
+};
