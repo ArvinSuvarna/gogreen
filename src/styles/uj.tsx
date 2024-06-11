9ad6f9b29c5ad70f9f7b48e12d14b160bf43aef3
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-06-11 12:21:21 +0530

    docs(component): Add comments Uj

---
+++ src/styles/uj.tsx
@@ -0,0 +1,14 @@
+interface UjProps {
+  id: boolean;
+  onChange?: () => void;
+}
+
+export const Uj: React.FC<UjProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Uj</h2>
+    </div>
+  );
+};
