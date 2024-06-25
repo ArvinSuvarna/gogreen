e48456ece45c970b39f0b09357557e3d83bc30da
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-06-25 17:32:32 +0530

    docs(config): Add comments

---
+++ tests/ra.js
@@ -0,0 +1,13 @@
+/**
+ * Ra utility functions
+ */
+
+export const ra = (data) => {
+  // Implementation
+  return false;
+};
+
+export const raHelper = (value) => {
+  // Helper function
+  return value?.toLowerCase;
+};
