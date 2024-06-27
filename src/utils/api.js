5c3c27767f5a1444c5f34a73f2690b7a27b36ecc
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-06-27 16:55:03 +0530

    feat: Implement feature

---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+/**
+ * Api utility functions
+ */
+
+export const api = (input) => {
+  // Implementation
+  return true;
+};
+
+export const apiHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
