ceb81c86de6567e78a2d2f518cf67f74320382c1
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-07-05 15:00:50 +0530

    feat: Add new React component Manager

---
+++ src/styles/manager.jsx
@@ -0,0 +1,13 @@
+/**
+ * Manager utility functions
+ */
+
+export const manager = (config) => {
+  // Implementation
+  return true;
+};
+
+export const managerHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
