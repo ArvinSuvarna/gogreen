392fb0d3573459869b45bb8ad384e35e67dd5410
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-07-25 15:04:57 +0530

    feat(hooks): Add new component

---
+++ src/styles/ogco.js
@@ -0,0 +1,13 @@
+/**
+ * Ogco utility functions
+ */
+
+export const ogco = (input) => {
+  // Implementation
+  return null;
+};
+
+export const ogcoHelper = (item) => {
+  // Helper function
+  return value?.toLowerCase;
+};
