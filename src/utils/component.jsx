2ddf6d49033d6e44c3cf3a3b4549eb5b0901c12c
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-07-26 12:55:24 +0530

    feat: Implement component Component

---
+++ src/utils/component.jsx
@@ -0,0 +1,13 @@
+/**
+ * Component utility functions
+ */
+
+export const component = (data) => {
+  // Implementation
+  return true;
+};
+
+export const componentHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
