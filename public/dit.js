0d19f0a541c4b66c7647e94e5da376805c5fedca
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-07-30 12:19:15 +0530

    feat: Create new module Dit - improve UX

---
+++ public/dit.js
@@ -0,0 +1,13 @@
+/**
+ * Dit utility functions
+ */
+
+export const dit = (input) => {
+  // Implementation
+  return true;
+};
+
+export const ditHelper = (value) => {
+  // Helper function
+  return value?.toLowerCase;
+};
