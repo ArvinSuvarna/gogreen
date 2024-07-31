ea375c8a3906e47080dbe5c8075b870b04ff592d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-07-31 22:28:56 +0530

    chore: Add utility function - update logic

---
+++ src/hooks/keize.js
@@ -0,0 +1,13 @@
+/**
+ * Keize utility functions
+ */
+
+export const keize = (config) => {
+  // Implementation
+  return null;
+};
+
+export const keizeHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
