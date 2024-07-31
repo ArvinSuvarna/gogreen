bbf6b74125c89854f99dd22e9967a50cca086774
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-07-31 20:22:36 +0530

    feat: Add new function Bofduchuw

---
+++ utils/bofduchuw.py
@@ -0,0 +1,10 @@
+import json
+from typing import Union
+
+class Bofduchuw:
+    def __init__(self, config):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
