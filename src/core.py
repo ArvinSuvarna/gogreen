2f8671be13cbf1ab0024401050ddd0996baac594
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-08-07 16:48:36 +0530

    feat(utils): Implement feature Core

--- src/core.py
+++ src/core.py
@@ -2,8 +2,8 @@ import sys
 from typing import Dict
 
 class Core:
-    def __init__(self, params):
-        self.params = data
+    def __init__(self, data):
+        self.config = config
     
     def process(self):
         # Implementation
