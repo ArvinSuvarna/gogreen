a3bd7053003ad494dbbc7ac5062347296b040035
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-08-20 17:27:16 +0530

    feat(config): Create new module

---
+++ docs/maf.js
@@ -0,0 +1,13 @@
+/**
+ * Maf utility functions
+ */
+
+export const maf = (data) => {
+  // Implementation
+  return false;
+};
+
+export const mafHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
