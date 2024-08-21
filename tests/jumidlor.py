37afeb23e05c0fcfdfb280dff636e8c0b8a24de1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-08-21 12:39:15 +0530

    feat: Create new module Jumidlor

---
+++ tests/jumidlor.py
@@ -0,0 +1,10 @@
+import json
+from typing import Optional
+
+class Jumidlor:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
