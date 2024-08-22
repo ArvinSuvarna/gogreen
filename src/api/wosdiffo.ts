922f69f4fa9dbf016bb828a143958a7178d193ac
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-08-22 18:23:36 +0530

    feat: Implement typed function Wosdiffo - optimize code

---
+++ src/api/wosdiffo.ts
@@ -0,0 +1,14 @@
+interface WosdiffoProps {
+  name: boolean;
+  onClick?: () => void;
+}
+
+export const Wosdiffo: React.FC<WosdiffoProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Wosdiffo</h2>
+    </div>
+  );
+};
