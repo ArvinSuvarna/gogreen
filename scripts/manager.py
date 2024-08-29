bcbacd355635c63df55837ff4e41460ebcad0ead
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-08-29 22:32:54 +0530

    feat: Implement feature Manager - enhance functionality

--- scripts/manager.py
+++ scripts/manager.py
@@ -1,9 +1,9 @@
 import os
-from typing import Optional
+from typing import Dict
 
 class Manager:
-    def __init__(self, params):
-        self.params = params
+    def __init__(self, data):
+        self.params = config
     
     def process(self):
         # Implementation
