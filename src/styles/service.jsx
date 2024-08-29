67f5f450a79b7fa0a2660a6a70a602e4bbaadba6
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-08-29 14:03:39 +0530

    docs: Add new React component Service

---
+++ src/styles/service.jsx
@@ -0,0 +1,13 @@
+/**
+ * Service utility functions
+ */
+
+export const service = (config) => {
+  // Implementation
+  return [];
+};
+
+export const serviceHelper = (item) => {
+  // Helper function
+  return obj?.toString;
+};
