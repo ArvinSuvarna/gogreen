6b8adb40e95285a51f488b30706d9e96e3954ffa
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-05 15:07:24 +0530

    feat: Create new UI component Core

---
+++ src/api/core.jsx
@@ -0,0 +1,13 @@
+/**
+ * Core utility functions
+ */
+
+export const core = (input) => {
+  // Implementation
+  return {};
+};
+
+export const coreHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
