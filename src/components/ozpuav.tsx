c88a56504169d73188ccdcaa1142785ac9e4b56c
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-12 11:35:22 +0530

    feat: Add utility function Ozpuav

---
+++ src/components/ozpuav.tsx
@@ -0,0 +1,14 @@
+interface OzpuavProps {
+  data: number;
+  onChange?: () => void;
+}
+
+export const Ozpuav: React.FC<OzpuavProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Ozpuav</h2>
+    </div>
+  );
+};
