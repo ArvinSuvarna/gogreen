1a4e04313380037c58d2d1460243c3c168ad7ae7
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-13 13:49:00 +0530

    feat(utils): Add type definitions

---
+++ src/api/epnalzit.ts
@@ -0,0 +1,14 @@
+interface EpnalzitProps {
+  name: boolean;
+  onClick?: () => void;
+}
+
+export const Epnalzit: React.FC<EpnalzitProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Epnalzit</h2>
+    </div>
+  );
+};
