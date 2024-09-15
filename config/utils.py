d819d064ad8c46ec432e55ee4e370ac5c6e6229c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-09-15 22:21:19 +0530

    chore: Create new module

--- config/utils.py
+++ config/utils.py
@@ -1,7 +1,10 @@
-def utils(params):
-    """
-    Utils function
-    """
-    result = None
-    # Process input_data
-    return result
+import sys
+from typing import Optional
+
+class Utils:
+    def __init__(self, config):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
