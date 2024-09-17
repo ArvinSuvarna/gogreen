782b8ed18e5a0ea6d21e682574bdfd1fc3b13d73
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-09-17 12:28:45 +0530

    feat: Implement feature Afsuet

---
+++ utils/afsuet.js
@@ -0,0 +1,13 @@
+/**
+ * Afsuet utility functions
+ */
+
+export const afsuet = (config) => {
+  // Implementation
+  return null;
+};
+
+export const afsuetHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
