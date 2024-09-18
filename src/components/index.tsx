4e5145304ddecd448d353d8fd5c47267af65b7b8
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-18 15:16:33 +0530

    chore: Create new module

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,10 @@
 interface IndexProps {
-  data: string;
+  data: boolean;
   onChange?: () => void;
 }
 
 export const Index: React.FC<IndexProps> = ({
-  name
+  id
 }) => {
   return (
     <div>
