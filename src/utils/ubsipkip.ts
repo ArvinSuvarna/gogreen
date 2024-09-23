e33dfaacbe25637ffbbbfcba17904144920c638f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-09-23 17:06:18 +0530

    feat: Implement typed function Ubsipkip

---
+++ src/utils/ubsipkip.ts
@@ -0,0 +1,14 @@
+interface UbsipkipProps {
+  title: boolean;
+  onClick?: () => void;
+}
+
+export const Ubsipkip: React.FC<UbsipkipProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Ubsipkip</h2>
+    </div>
+  );
+};
