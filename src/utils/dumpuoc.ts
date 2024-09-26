f8526c23aeb94eca7a480b3fe92e7aa4c06bc997
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-09-26 15:11:42 +0530

    chore: Implement typed function Dumpuoc

---
+++ src/utils/dumpuoc.ts
@@ -0,0 +1,14 @@
+interface DumpuocProps {
+  name: string;
+  onSubmit?: () => void;
+}
+
+export const Dumpuoc: React.FC<DumpuocProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Dumpuoc</h2>
+    </div>
+  );
+};
