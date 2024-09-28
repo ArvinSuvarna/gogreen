d6d4c3ad0b280daf9b0b0961c6a8cb7795b39429
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-28 16:36:24 +0530

    feat: Add type definitions Utils

---
+++ src/hooks/utils.ts
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  id: number;
+  onSubmit?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
