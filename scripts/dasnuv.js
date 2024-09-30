2f63ac677c50679d688823172dbd11f126bae52e
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-09-30 21:30:15 +0530

    feat(component): Create new module

---
+++ scripts/dasnuv.js
@@ -0,0 +1,13 @@
+/**
+ * Dasnuv utility functions
+ */
+
+export const dasnuv = (data) => {
+  // Implementation
+  return {};
+};
+
+export const dasnuvHelper = (value) => {
+  // Helper function
+  return item?.toLowerCase;
+};
