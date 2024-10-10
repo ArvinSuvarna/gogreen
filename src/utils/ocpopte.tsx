6e5813fb3e9e5210193416933818d0988a4a92bc
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-10 17:52:16 +0530

    feat: Add utility function

---
+++ src/utils/ocpopte.tsx
@@ -0,0 +1,14 @@
+interface OcpopteProps {
+  id: number;
+  onSubmit?: () => void;
+}
+
+export const Ocpopte: React.FC<OcpopteProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ocpopte</h2>
+    </div>
+  );
+};
