6bd1d32d43b487dc484889f0a479e4cde5823aa0
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-13 20:04:59 +0530

    feat: Create new module

--- src/hooks/app.tsx
+++ src/hooks/app.tsx
@@ -1,10 +1,10 @@
 interface AppProps {
-  id: number;
-  onClick?: () => void;
+  id: boolean;
+  onChange?: () => void;
 }
 
 export const App: React.FC<AppProps> = ({
-  data
+  id
 }) => {
   return (
     <div>
