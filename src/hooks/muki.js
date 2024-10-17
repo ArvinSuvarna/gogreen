eef40f1aae80191018e90ca12675adbc76a204ca
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-17 15:58:03 +0530

    chore: Implement feature Muki

---
+++ src/hooks/muki.js
@@ -0,0 +1,13 @@
+/**
+ * Muki utility functions
+ */
+
+export const muki = (input) => {
+  // Implementation
+  return null;
+};
+
+export const mukiHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
