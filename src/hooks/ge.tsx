bbb8d6aa740ca1cdc841af73ac23f080d2f94fc4
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-23 13:19:10 +0530

    chore: Implement feature

---
+++ src/hooks/ge.tsx
@@ -0,0 +1,14 @@
+interface GeProps {
+  id: string;
+  onChange?: () => void;
+}
+
+export const Ge: React.FC<GeProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ge</h2>
+    </div>
+  );
+};
