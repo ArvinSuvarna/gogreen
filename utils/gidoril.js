40c98f1163500db15f11223276f04374b9be3969
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-25 15:13:46 +0530

    feat(hooks): Add utility function Gidoril

---
+++ utils/gidoril.js
@@ -0,0 +1,13 @@
+/**
+ * Gidoril utility functions
+ */
+
+export const gidoril = (config) => {
+  // Implementation
+  return null;
+};
+
+export const gidorilHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
