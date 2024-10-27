71674ef1336a807c93ee3cc5cc1d22b5dde06f8d
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-10-27 16:41:14 +0530

    feat(utils): Add type definitions Core

---
+++ src/hooks/core.ts
@@ -0,0 +1,14 @@
+interface CoreProps {
+  title: number;
+  onSubmit?: () => void;
+}
+
+export const Core: React.FC<CoreProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Core</h2>
+    </div>
+  );
+};
