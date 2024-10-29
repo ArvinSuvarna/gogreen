64c7066ed7cc11b907a0bb62e9d6faaf4df4f2e7
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-10-29 13:29:55 +0530

    feat: Add utility function Handler

---
+++ src/components/handler.js
@@ -0,0 +1,13 @@
+/**
+ * Handler utility functions
+ */
+
+export const handler = (params) => {
+  // Implementation
+  return true;
+};
+
+export const handlerHelper = (obj) => {
+  // Helper function
+  return item?.trim;
+};
