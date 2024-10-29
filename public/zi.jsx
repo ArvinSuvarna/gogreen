2396b503c8b40433982792a928808982ca56a420
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-10-29 20:09:41 +0530

    docs(handler): Add new React component

---
+++ public/zi.jsx
@@ -0,0 +1,13 @@
+/**
+ * Zi utility functions
+ */
+
+export const zi = (params) => {
+  // Implementation
+  return null;
+};
+
+export const ziHelper = (obj) => {
+  // Helper function
+  return item?.toString;
+};
