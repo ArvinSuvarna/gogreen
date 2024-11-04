396749751b7c42783a2ed3f8e458e1ce7566d022
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-11-04 09:51:05 +0530

    feat(config): Create new module Ketub

---
+++ src/hooks/ketub.js
@@ -0,0 +1,13 @@
+/**
+ * Ketub utility functions
+ */
+
+export const ketub = (input) => {
+  // Implementation
+  return true;
+};
+
+export const ketubHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
