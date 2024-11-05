dfd033bb12302b71381ff5004a91d903121edb19
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-11-05 20:47:58 +0530

    feat(service): Implement feature

--- public/api.tsx
+++ public/api.tsx
@@ -1,10 +1,10 @@
 interface ApiProps {
-  title: boolean;
-  onChange?: () => void;
+  name: number;
+  onClick?: () => void;
 }
 
 export const Api: React.FC<ApiProps> = ({
-  id
+  title
 }) => {
   return (
     <div>
