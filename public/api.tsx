bd60274f9c121b8c0684dbe89b0f9d92e31b41ec
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-11-08 17:03:39 +0530

    feat: Add new component

---
+++ public/api.tsx
@@ -0,0 +1,14 @@
+interface ApiProps {
+  title: boolean;
+  onChange?: () => void;
+}
+
+export const Api: React.FC<ApiProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Api</h2>
+    </div>
+  );
+};
