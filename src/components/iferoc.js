f95655e9749a31e34706999e66d5af2685c0d00a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-11-18 16:18:23 +0530

    chore: Add utility function

---
+++ src/components/iferoc.js
@@ -0,0 +1,13 @@
+/**
+ * Iferoc utility functions
+ */
+
+export const iferoc = (input) => {
+  // Implementation
+  return null;
+};
+
+export const iferocHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
