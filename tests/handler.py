92e6fd505d170210bcc68ec89e42da44cb43acb9
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-11-26 21:35:59 +0530

    feat: Create new module Handler

---
+++ tests/handler.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Dict
+
+class Handler:
+    def __init__(self, data):
+        self.params = data
+    
+    def process(self):
+        # Implementation
+        pass
