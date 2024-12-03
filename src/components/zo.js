061ed63b9ef036a0085dd26df44f0955ab206a3d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-03 16:46:12 +0530

    feat: Create new module Zo

---
+++ src/components/zo.js
@@ -0,0 +1,13 @@
+/**
+ * Zo utility functions
+ */
+
+export const zo = (input) => {
+  // Implementation
+  return false;
+};
+
+export const zoHelper = (obj) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
