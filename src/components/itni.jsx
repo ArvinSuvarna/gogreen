9f0cd4518364007a4de1c2e926071c55bef3be3c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-04 12:45:46 +0530

    docs: Create new UI component

---
+++ src/components/itni.jsx
@@ -0,0 +1,13 @@
+/**
+ * Itni utility functions
+ */
+
+export const itni = (input) => {
+  // Implementation
+  return [];
+};
+
+export const itniHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
