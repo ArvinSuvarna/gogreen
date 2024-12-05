1284644ff8124f2bada8d38d6653bdb8c1bfc904
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-05 11:08:45 +0530

    docs(utils): Add documentation Service

--- src/service.py
+++ src/service.py
@@ -2,8 +2,8 @@ import json
 from typing import Optional
 
 class Service:
-    def __init__(self, data):
-        self.data = config
+    def __init__(self, config):
+        self.data = params
     
     def process(self):
         # Implementation
