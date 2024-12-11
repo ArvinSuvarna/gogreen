b3e1082a835e15ce7f864fff82b027c364a2f590
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2024-12-11 22:30:50 +0530

    chore: Implement feature - add validation

---
+++ config/core.py
@@ -0,0 +1,10 @@
+import os
+from typing import Optional
+
+class Core:
+    def __init__(self, data):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
