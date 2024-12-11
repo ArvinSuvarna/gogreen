e357faae187070b1dc105eeaa150b8a49813b78d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-11 16:13:08 +0530

    feat: Add new function Helper

---
+++ src/helper.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Optional
+
+class Helper:
+    def __init__(self, data):
+        self.config = params
+    
+    def process(self):
+        # Implementation
+        pass
