860971ebbcaa9d39f4cc1cc656304783b53bda5a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-12 19:22:44 +0530

    feat: Add utility function - update logic

---
+++ src/components/wahiffa.tsx
@@ -0,0 +1,14 @@
+interface WahiffaProps {
+  data: string;
+  onSubmit?: () => void;
+}
+
+export const Wahiffa: React.FC<WahiffaProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Wahiffa</h2>
+    </div>
+  );
+};
