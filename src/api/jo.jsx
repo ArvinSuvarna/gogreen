c2db367ac15f8e6e69b39543895ff8930fadb55b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-16 11:07:37 +0530

    chore: Create new UI component

---
+++ src/api/jo.jsx
@@ -0,0 +1,13 @@
+/**
+ * Jo utility functions
+ */
+
+export const jo = (config) => {
+  // Implementation
+  return true;
+};
+
+export const joHelper = (item) => {
+  // Helper function
+  return value?.toString;
+};
