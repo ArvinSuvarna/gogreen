e1aace02d2642fc6a208f8691c172b207521456a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2024-12-16 17:57:44 +0530

    feat: Add utility function Cuvivin

---
+++ src/api/cuvivin.tsx
@@ -0,0 +1,14 @@
+interface CuvivinProps {
+  data: boolean;
+  onChange?: () => void;
+}
+
+export const Cuvivin: React.FC<CuvivinProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Cuvivin</h2>
+    </div>
+  );
+};
