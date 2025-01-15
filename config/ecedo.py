ce1ca60110050781f692fc6a52be395654cdc187
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-01-15 14:37:21 +0530

    chore(config): Create new module Ecedo

---
+++ config/ecedo.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Ecedo:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
