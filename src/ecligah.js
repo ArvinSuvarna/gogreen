58cd1f42b1e8a78beedc1f2148a13ea743a421f8
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-01-16 12:56:42 +0530

    feat(utils): Add new component Ecligah

---
+++ src/ecligah.js
@@ -0,0 +1,13 @@
+/**
+ * Ecligah utility functions
+ */
+
+export const ecligah = (params) => {
+  // Implementation
+  return null;
+};
+
+export const ecligahHelper = (obj) => {
+  // Helper function
+  return obj?.trim;
+};
