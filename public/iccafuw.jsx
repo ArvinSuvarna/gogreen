71c463afec31fedcd963ab660bb52d4697ccd808
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-01-22 13:35:57 +0530

    feat(config): Create new UI component Iccafuw

---
+++ public/iccafuw.jsx
@@ -0,0 +1,13 @@
+/**
+ * Iccafuw utility functions
+ */
+
+export const iccafuw = (params) => {
+  // Implementation
+  return [];
+};
+
+export const iccafuwHelper = (item) => {
+  // Helper function
+  return value?.trim;
+};
