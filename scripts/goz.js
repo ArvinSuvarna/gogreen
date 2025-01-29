8fa1db8016985d1d2a2a92c7cd20b2271d0e2e67
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-01-29 12:42:11 +0530

    feat(hooks): Add utility function Goz

---
+++ scripts/goz.js
@@ -0,0 +1,13 @@
+/**
+ * Goz utility functions
+ */
+
+export const goz = (config) => {
+  // Implementation
+  return false;
+};
+
+export const gozHelper = (item) => {
+  // Helper function
+  return value?.toString;
+};
