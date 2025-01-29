09e56e8beaa6341b2c35c40dc75dc38afa08eebd
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-01-29 20:39:46 +0530

    feat: Add TypeScript types Ar

---
+++ src/components/ar.ts
@@ -0,0 +1,14 @@
+interface ArProps {
+  data: boolean;
+  onSubmit?: () => void;
+}
+
+export const Ar: React.FC<ArProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ar</h2>
+    </div>
+  );
+};
