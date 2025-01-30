894820057d6d1c6ccdfa31d2282b291164dfd1a9
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-01-30 14:44:17 +0530

    feat(styles): Implement typed function

---
+++ src/styles/pir.ts
@@ -0,0 +1,14 @@
+interface PirProps {
+  id: string;
+  onSubmit?: () => void;
+}
+
+export const Pir: React.FC<PirProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Pir</h2>
+    </div>
+  );
+};
