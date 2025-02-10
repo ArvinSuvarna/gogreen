9c94f3f600b0cb3d10cb2ab6323be37584b9accc
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-02-10 12:49:47 +0530

    feat(hooks): Create new module

---
+++ utils/ecohi.js
@@ -0,0 +1,13 @@
+/**
+ * Ecohi utility functions
+ */
+
+export const ecohi = (input) => {
+  // Implementation
+  return {};
+};
+
+export const ecohiHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
