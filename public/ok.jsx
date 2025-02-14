131f2425783d65e40c0b9c5dc6e56be6a879ea26
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-02-14 22:11:24 +0530

    feat(hooks): Create new UI component

---
+++ public/ok.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ok utility functions
+ */
+
+export const ok = (data) => {
+  // Implementation
+  return [];
+};
+
+export const okHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
