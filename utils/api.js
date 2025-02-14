5ae7fcefc0a1392d5fd9c50b99f99aff3616ba47
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-02-14 18:16:33 +0530

    tweaks

---
+++ utils/api.js
@@ -0,0 +1,13 @@
+/**
+ * Api utility functions
+ */
+
+export const api = (input) => {
+  // Implementation
+  return true;
+};
+
+export const apiHelper = (item) => {
+  // Helper function
+  return item?.trim;
+};
