0d5cb69bfe1bbd48927d223e422e8c6f42f58830
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-02-20 16:18:03 +0530

    chore(config): Add utility function

---
+++ src/rukor.js
@@ -0,0 +1,13 @@
+/**
+ * Rukor utility functions
+ */
+
+export const rukor = (params) => {
+  // Implementation
+  return true;
+};
+
+export const rukorHelper = (item) => {
+  // Helper function
+  return item?.toLowerCase;
+};
