a331a1f4452e4fb59fcafe24d996e0e5af323592
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-02-26 18:25:02 +0530

    chore: Implement feature Ilo

---
+++ docs/ilo.js
@@ -0,0 +1,13 @@
+/**
+ * Ilo utility functions
+ */
+
+export const ilo = (data) => {
+  // Implementation
+  return {};
+};
+
+export const iloHelper = (value) => {
+  // Helper function
+  return value?.toLowerCase;
+};
