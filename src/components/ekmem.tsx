0d9b644068ea9af678891f5168e6ead5b13f408a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-10 21:02:05 +0530

    updates

---
+++ src/components/ekmem.tsx
@@ -0,0 +1,14 @@
+interface EkmemProps {
+  title: boolean;
+  onChange?: () => void;
+}
+
+export const Ekmem: React.FC<EkmemProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Ekmem</h2>
+    </div>
+  );
+};
