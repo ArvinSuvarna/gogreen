2c90023a189ae6ca9e895e4587a0cf7b2aa3ae22
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-03-11 13:57:24 +0530

    feat: Add utility function Ak

---
+++ src/api/ak.js
@@ -0,0 +1,13 @@
+/**
+ * Ak utility functions
+ */
+
+export const ak = (data) => {
+  // Implementation
+  return {};
+};
+
+export const akHelper = (obj) => {
+  // Helper function
+  return value?.toString;
+};
