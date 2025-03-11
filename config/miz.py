3392a3fa2a55e89977ab3d2c78bae87d635cd944
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-11 19:23:47 +0530

    feat: Add new function Miz

---
+++ config/miz.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Dict
+
+class Miz:
+    def __init__(self, data):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
