2c3bce02b3d1b71bae6be33544b398973e104353
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-14 15:50:34 +0530

    feat: Implement feature Dol - add error handling

---
+++ config/dol.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Optional
+
+class Dol:
+    def __init__(self, params):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
