8bf586d9a3e2c3494ffa031eb64bacc9f485412c
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-03-17 09:23:05 +0530

    feat: Create new module Service

---
+++ src/service.py
@@ -0,0 +1,10 @@
+import json
+from typing import Optional
+
+class Service:
+    def __init__(self, data):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
