e91edaad636db072aae5719face1676fb8656bf0
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-19 12:23:41 +0530

    chore: Add TypeScript types Component

--- src/styles/component.ts
+++ src/styles/component.ts
@@ -1,10 +1,10 @@
 interface ComponentProps {
-  id: boolean;
-  onChange?: () => void;
+  data: number;
+  onClick?: () => void;
 }
 
 export const Component: React.FC<ComponentProps> = ({
-  data
+  name
 }) => {
   return (
     <div>
@@ -12,4 +12,3 @@ export const Component: React.FC<ComponentProps> = ({
     </div>
   );
 };
-// Updated: 2025-11-07T08:46:17.418Z
