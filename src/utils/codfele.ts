c7eb51b5d415f87965fb099f6c54c46357a36283
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-25 21:22:22 +0530

    chore: Add type definitions Codfele

---
+++ src/utils/codfele.ts
@@ -0,0 +1,14 @@
+interface CodfeleProps {
+  data: string;
+  onChange?: () => void;
+}
+
+export const Codfele: React.FC<CodfeleProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Codfele</h2>
+    </div>
+  );
+};
