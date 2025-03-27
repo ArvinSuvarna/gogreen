e0e024f00e288cbd87ceefd354c8993a0e0a0508
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-03-27 21:35:01 +0530

    feat(hooks): Add TypeScript types

---
+++ src/api/service.ts
@@ -0,0 +1,14 @@
+interface ServiceProps {
+  name: boolean;
+  onClick?: () => void;
+}
+
+export const Service: React.FC<ServiceProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Service</h2>
+    </div>
+  );
+};
