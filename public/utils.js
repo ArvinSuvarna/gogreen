6ef9d94fc07b1961800ef522557a6dce2cf0c5e3
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-03-27 15:16:52 +0530

    feat: Add utility function Utils

---
+++ public/utils.js
@@ -0,0 +1,13 @@
+/**
+ * Utils utility functions
+ */
+
+export const utils = (data) => {
+  // Implementation
+  return false;
+};
+
+export const utilsHelper = (obj) => {
+  // Helper function
+  return value?.toString;
+};
