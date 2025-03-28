0170fce4d7b585cb1ec350f65e0764b0557a1730
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-03-28 16:37:15 +0530

    feat: Implement typed function

---
+++ src/styles/utils.ts
@@ -0,0 +1,14 @@
+interface UtilsProps {
+  name: number;
+  onSubmit?: () => void;
+}
+
+export const Utils: React.FC<UtilsProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Utils</h2>
+    </div>
+  );
+};
