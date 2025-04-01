79b7f471137eabcd3983e109b4ab224eb64e9be2
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-01 12:15:56 +0530

    feat: Create new UI component Pa

---
+++ src/utils/pa.jsx
@@ -0,0 +1,13 @@
+/**
+ * Pa utility functions
+ */
+
+export const pa = (input) => {
+  // Implementation
+  return true;
+};
+
+export const paHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
