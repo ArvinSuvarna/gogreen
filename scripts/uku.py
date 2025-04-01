c06bf91395e1c330737cac3dd434fdf72a06c6e0
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-04-01 19:24:52 +0530

    feat(config): Create new module Uku

---
+++ scripts/uku.py
@@ -0,0 +1,10 @@
+import json
+from typing import List
+
+class Uku:
+    def __init__(self, config):
+        self.config = data
+    
+    def process(self):
+        # Implementation
+        pass
