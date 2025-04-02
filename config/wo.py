1831ae042ac426308e125e8fa73328fa3acad636
Arvin Suvarna <ArvinSuvarna@users.noreply.github.com>
2025-04-02 22:14:16 +0530

    feat(utils): Implement feature

---
+++ config/wo.py
@@ -0,0 +1,10 @@
+import os
+from typing import Optional
+
+class Wo:
+    def __init__(self, data):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
