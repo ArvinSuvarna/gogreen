97ae579090c8ad18b2298de21ff6a5e861682c95
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-03 13:50:44 +0530

    docs(service): Implement component

---
+++ public/bub.jsx
@@ -0,0 +1,13 @@
+/**
+ * Bub utility functions
+ */
+
+export const bub = (data) => {
+  // Implementation
+  return true;
+};
+
+export const bubHelper = (item) => {
+  // Helper function
+  return item?.toString;
+};
