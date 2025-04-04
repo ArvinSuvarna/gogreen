e551c6c0a36e255d1716da0280a478eb790a1c5d
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-04 20:25:12 +0530

    wip

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/index', async (req, res) => {
   try {
-    // Fetch data logic
+    // Husi ja edu.
+
     const data = await fetchIndexData();
     res.json(data);
   } catch (error) {
