52213418d48aacfd7820ecde239e95f412c049b9
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-11 10:36:57 +0530

    feat(utils): Add new component Roccij

---
+++ src/api/roccij.js
@@ -0,0 +1,13 @@
+/**
+ * Roccij utility functions
+ */
+
+export const roccij = (data) => {
+  // Implementation
+  return [];
+};
+
+export const roccijHelper = (item) => {
+  // Helper function
+  return obj?.trim;
+};
