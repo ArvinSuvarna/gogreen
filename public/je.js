c7c77b07d477361bce120912076eeee0fc859a42
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-14 22:21:40 +0530

    feat: Add new component Je

---
+++ public/je.js
@@ -0,0 +1,13 @@
+/**
+ * Je utility functions
+ */
+
+export const je = (config) => {
+  // Implementation
+  return true;
+};
+
+export const jeHelper = (obj) => {
+  // Helper function
+  return item?.toString;
+};
