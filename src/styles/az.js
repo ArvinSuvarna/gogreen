d535ba7a99c813be3fd0d88e73c1f84ab5ee2608
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-21 10:55:27 +0530

    feat: Create new module Az

---
+++ src/styles/az.js
@@ -0,0 +1,13 @@
+/**
+ * Az utility functions
+ */
+
+export const az = (config) => {
+  // Implementation
+  return [];
+};
+
+export const azHelper = (value) => {
+  // Helper function
+  return item?.toString;
+};
