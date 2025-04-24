5cd18c3718074296c3f5ec0eec2e7a0e1340e05b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-24 13:48:12 +0530

    feat(component): Add utility function Fi

---
+++ src/components/fi.js
@@ -0,0 +1,13 @@
+/**
+ * Fi utility functions
+ */
+
+export const fi = (config) => {
+  // Implementation
+  return true;
+};
+
+export const fiHelper = (value) => {
+  // Helper function
+  return obj?.toString;
+};
