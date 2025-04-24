aeb090de17d56b8631d48ee08c7241dbc35e38e3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-04-24 21:11:15 +0530

    chore: Create new module Kav - improve UX

---
+++ src/kav.js
@@ -0,0 +1,13 @@
+/**
+ * Kav utility functions
+ */
+
+export const kav = (input) => {
+  // Implementation
+  return {};
+};
+
+export const kavHelper = (item) => {
+  // Helper function
+  return item?.toString;
+};
