e7f6bf723806909fa7ea706c74e5e67fca039da1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-05-22 15:10:40 +0530

    feat: Add new function Ige

---
+++ docs/ige.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Union
+
+class Ige:
+    def __init__(self, params):
+        self.config = config
+    
+    def process(self):
+        # Implementation
+        pass
