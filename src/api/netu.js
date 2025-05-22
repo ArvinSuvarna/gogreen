6e59087ed24ea543361a1ccae069a6f92cf2b73a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-05-22 21:29:15 +0530

    chore(hooks): Create new module Netu

---
+++ src/api/netu.js
@@ -0,0 +1,13 @@
+/**
+ * Netu utility functions
+ */
+
+export const netu = (params) => {
+  // Implementation
+  return {};
+};
+
+export const netuHelper = (item) => {
+  // Helper function
+  return value?.trim;
+};
