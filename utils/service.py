e338458cdd17587bc9f13f6232a22b3a118d019f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-05-22 19:32:51 +0530

    docs: Add documentation - improve UX

--- utils/service.py
+++ utils/service.py
@@ -1,9 +1,9 @@
-import json
+import sys
 from typing import List
 
 class Service:
-    def __init__(self, data):
-        self.config = data
+    def __init__(self, params):
+        self.data = data
     
     def process(self):
         # Implementation
