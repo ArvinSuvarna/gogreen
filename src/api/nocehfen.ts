adf0437eff4f9e69c0e1d6d94cd770481a9d1cbd
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-05-28 17:46:54 +0530

    chore: Add type definitions Nocehfen

---
+++ src/api/nocehfen.ts
@@ -0,0 +1,14 @@
+interface NocehfenProps {
+  title: number;
+  onClick?: () => void;
+}
+
+export const Nocehfen: React.FC<NocehfenProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Nocehfen</h2>
+    </div>
+  );
+};
