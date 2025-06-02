e2468a5cc4f8425597659b61a3adfcd12091ca20
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-06-02 15:43:01 +0530

    feat: Implement typed function

---
+++ src/styles/wimfeva.ts
@@ -0,0 +1,14 @@
+interface WimfevaProps {
+  id: number;
+  onClick?: () => void;
+}
+
+export const Wimfeva: React.FC<WimfevaProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Wimfeva</h2>
+    </div>
+  );
+};
