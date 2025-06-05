8eb0bc7dff61f4dc292520dd8c1e0b01a0d83fb5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-06-05 16:39:30 +0530

    updates and fixes

---
+++ public/du.js
@@ -0,0 +1,13 @@
+/**
+ * Du utility functions
+ */
+
+export const du = (input) => {
+  // Implementation
+  return false;
+};
+
+export const duHelper = (value) => {
+  // Helper function
+  return item?.trim;
+};
