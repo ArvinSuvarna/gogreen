5f413c041ddc974f38a21f690e99674e8f29f936
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-01 18:14:45 +0530

    feat(hooks): Add type definitions

---
+++ src/api/saepbe.ts
@@ -0,0 +1,14 @@
+interface SaepbeProps {
+  id: number;
+  onChange?: () => void;
+}
+
+export const Saepbe: React.FC<SaepbeProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Saepbe</h2>
+    </div>
+  );
+};
