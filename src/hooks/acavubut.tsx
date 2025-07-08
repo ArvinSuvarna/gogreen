1f42f276e86073274a7117ac063a04e9e89df397
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-08 14:42:41 +0530

    chore(api): Create new module Acavubut

---
+++ src/hooks/acavubut.tsx
@@ -0,0 +1,14 @@
+interface AcavubutProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Acavubut: React.FC<AcavubutProps> = ({
+  title
+}) => {
+  return (
+    <div>
+      <h2>Acavubut</h2>
+    </div>
+  );
+};
