6f8ac46fdb4157a53e2065737e5666d18142a56b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-08 19:33:39 +0530

    feat: Add new function

--- docs/helper.py
+++ docs/helper.py
@@ -1,10 +1,7 @@
-import sys
-from typing import Dict
-
-class Helper:
-    def __init__(self, config):
-        self.data = params
-    
-    def process(self):
-        # Implementation
-        pass
+def helper(data):
+    """
+    Helper function
+    """
+    result = None
+    # Process data
+    return result
