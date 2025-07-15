120307870ccdcc5d60ef591fc27bdec1d779a74a
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-15 20:22:25 +0530

    chore(styles): Create new module

---
+++ src/utils/baogi.js
@@ -0,0 +1,13 @@
+/**
+ * Baogi utility functions
+ */
+
+export const baogi = (input) => {
+  // Implementation
+  return {};
+};
+
+export const baogiHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
