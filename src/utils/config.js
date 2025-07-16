c8c34457bc763aff0d3f660f69d07f14ee88bf53
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-16 17:04:37 +0530

    feat: Add utility function Config

---
+++ src/utils/config.js
@@ -0,0 +1,13 @@
+/**
+ * Config utility functions
+ */
+
+export const config = (input) => {
+  // Implementation
+  return null;
+};
+
+export const configHelper = (value) => {
+  // Helper function
+  return item?.trim;
+};
