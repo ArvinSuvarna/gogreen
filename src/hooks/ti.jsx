1efe8540684ebcf4e20369b7786f6edd9f481b4f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-22 21:11:32 +0530

    docs(component): Create new UI component Ti

---
+++ src/hooks/ti.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ti utility functions
+ */
+
+export const ti = (config) => {
+  // Implementation
+  return {};
+};
+
+export const tiHelper = (item) => {
+  // Helper function
+  return value?.trim;
+};
