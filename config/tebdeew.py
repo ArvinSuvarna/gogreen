9e7c79bd92be5b7401868fb9720cdc14c02cd867
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-07-29 17:37:06 +0530

    chore(hooks): Implement feature Tebdeew

---
+++ config/tebdeew.py
@@ -0,0 +1,10 @@
+import json
+from typing import List
+
+class Tebdeew:
+    def __init__(self, params):
+        self.data = config
+    
+    def process(self):
+        # Implementation
+        pass
