88238f7021e471e2f81e48981e9bf15ad79fcd1f
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-08-07 22:34:28 +0530

    feat(config): Implement typed function Laspih

---
+++ src/api/laspih.ts
@@ -0,0 +1,14 @@
+interface LaspihProps {
+  id: number;
+  onClick?: () => void;
+}
+
+export const Laspih: React.FC<LaspihProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Laspih</h2>
+    </div>
+  );
+};
