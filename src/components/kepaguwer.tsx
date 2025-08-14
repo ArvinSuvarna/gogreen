9a5471392889d331090cad1e53f29b74213867e6
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-08-14 13:06:54 +0530

    feat: Add utility function - improve performance

---
+++ src/components/kepaguwer.tsx
@@ -0,0 +1,14 @@
+interface KepaguwerProps {
+  id: boolean;
+  onChange?: () => void;
+}
+
+export const Kepaguwer: React.FC<KepaguwerProps> = ({
+  id
+}) => {
+  return (
+    <div>
+      <h2>Kepaguwer</h2>
+    </div>
+  );
+};
