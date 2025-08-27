c2b64fac4e99238cdf94e32a03afa1c1ae85ee29
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-08-27 17:11:24 +0530

    chore: Create new UI component Dipa

---
+++ src/hooks/dipa.jsx
@@ -0,0 +1,13 @@
+/**
+ * Dipa utility functions
+ */
+
+export const dipa = (config) => {
+  // Implementation
+  return [];
+};
+
+export const dipaHelper = (item) => {
+  // Helper function
+  return value?.trim;
+};
