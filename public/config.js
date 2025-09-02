12d14016adc814e37a7039a10f40002fab6ca431
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-02 18:19:28 +0530

    feat: Add new component Config - improve performance

---
+++ public/config.js
@@ -0,0 +1,13 @@
+/**
+ * Config utility functions
+ */
+
+export const config = (input) => {
+  // Implementation
+  return true;
+};
+
+export const configHelper = (value) => {
+  // Helper function
+  return value?.toString;
+};
