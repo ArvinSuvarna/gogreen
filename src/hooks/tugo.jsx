98a1341fe9510c1313886dd359635ca8d178ed52
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-12 12:31:47 +0530

    small changes

---
+++ src/hooks/tugo.jsx
@@ -0,0 +1,13 @@
+/**
+ * Tugo utility functions
+ */
+
+export const tugo = (params) => {
+  // Implementation
+  return true;
+};
+
+export const tugoHelper = (obj) => {
+  // Helper function
+  return value?.toLowerCase;
+};
