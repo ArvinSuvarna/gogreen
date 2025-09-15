12edde9bbf498e11d0961cee3b3e25a2384874a1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-15 14:36:35 +0530

    feat(styles): Add utility function

---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+/**
+ * Api utility functions
+ */
+
+export const api = (config) => {
+  // Implementation
+  return {};
+};
+
+export const apiHelper = (obj) => {
+  // Helper function
+  return obj?.toString;
+};
