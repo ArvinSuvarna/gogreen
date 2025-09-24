1c3b0f50b552b3e4f4507acce8b14ceafbce64f1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-24 19:59:14 +0530

    minor update

---
+++ src/components/urosacse.tsx
@@ -0,0 +1,14 @@
+interface UrosacseProps {
+  id: boolean;
+  onSubmit?: () => void;
+}
+
+export const Urosacse: React.FC<UrosacseProps> = ({
+  name
+}) => {
+  return (
+    <div>
+      <h2>Urosacse</h2>
+    </div>
+  );
+};
