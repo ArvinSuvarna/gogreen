8480e4816903599c58a55eedf3fa825205dbad6e
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-27 19:20:41 +0530

    chore: Create new module Radsacjo

---
+++ utils/radsacjo.py
@@ -0,0 +1,10 @@
+import datetime
+from typing import Dict
+
+class Radsacjo:
+    def __init__(self, params):
+        self.data = params
+    
+    def process(self):
+        # Implementation
+        pass
