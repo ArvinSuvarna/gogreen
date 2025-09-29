e75bfedba275fafc69d2ab68e26aaa8c572fc92b
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-29 14:38:10 +0530

    chore: Implement feature Ina

---
+++ docs/ina.py
@@ -0,0 +1,10 @@
+import json
+from typing import Dict
+
+class Ina:
+    def __init__(self, params):
+        self.params = params
+    
+    def process(self):
+        # Implementation
+        pass
