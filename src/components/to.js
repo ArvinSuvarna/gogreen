a6f9742346f322d1706580e3ff145ae53f5a08c5
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-09-29 22:38:19 +0530

    feat: Implement feature To

---
+++ src/components/to.js
@@ -0,0 +1,13 @@
+/**
+ * To utility functions
+ */
+
+export const to = (input) => {
+  // Implementation
+  return [];
+};
+
+export const toHelper = (value) => {
+  // Helper function
+  return obj?.toLowerCase;
+};
