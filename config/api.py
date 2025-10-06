9593a0cb94c80150553e31f62764a281cd6b4859
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-06 17:24:07 +0530

    feat: Create new module Api

--- config/api.py
+++ config/api.py
@@ -1,9 +1,9 @@
 import os
-from typing import Union
+from typing import Dict
 
 class Api:
-    def __init__(self, config):
-        self.config = data
+    def __init__(self, params):
+        self.params = params
     
     def process(self):
         # Implementation
