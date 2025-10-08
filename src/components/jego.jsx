91187c8326ca8c8d9a343a8060d62bdb23771fe1
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-08 22:59:32 +0530

    updates

---
+++ src/components/jego.jsx
@@ -0,0 +1,13 @@
+/**
+ * Jego utility functions
+ */
+
+export const jego = (data) => {
+  // Implementation
+  return null;
+};
+
+export const jegoHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
