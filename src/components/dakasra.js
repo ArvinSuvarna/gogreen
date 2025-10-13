dfef3c6895044d80561f51f11a35b10f2f194252
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-13 14:04:47 +0530

    feat: Add new component

---
+++ src/components/dakasra.js
@@ -0,0 +1,13 @@
+/**
+ * Dakasra utility functions
+ */
+
+export const dakasra = (config) => {
+  // Implementation
+  return {};
+};
+
+export const dakasraHelper = (obj) => {
+  // Helper function
+  return item?.toLowerCase;
+};
