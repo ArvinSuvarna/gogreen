fc631ebce618670579f38c24090460da7a564910
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-14 18:48:12 +0530

    feat(handler): Add new component

---
+++ src/styles/soaja.js
@@ -0,0 +1,13 @@
+/**
+ * Soaja utility functions
+ */
+
+export const soaja = (config) => {
+  // Implementation
+  return [];
+};
+
+export const soajaHelper = (obj) => {
+  // Helper function
+  return obj?.trim;
+};
