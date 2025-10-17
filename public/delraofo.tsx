2a92c21dd4ed583b22d5233972758c7473f73785
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-17 15:29:09 +0530

    chore: Add new component Delraofo - add error handling

---
+++ public/delraofo.tsx
@@ -0,0 +1,14 @@
+interface DelraofoProps {
+  title: number;
+  onChange?: () => void;
+}
+
+export const Delraofo: React.FC<DelraofoProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Delraofo</h2>
+    </div>
+  );
+};
