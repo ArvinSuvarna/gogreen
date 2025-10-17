c81ff220489f495d157f0ab5033a7b10387ebb31
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-17 09:10:00 +0530

    feat(api): Add type definitions

---
+++ src/utils/adwi.ts
@@ -0,0 +1,14 @@
+interface AdwiProps {
+  name: string;
+  onSubmit?: () => void;
+}
+
+export const Adwi: React.FC<AdwiProps> = ({
+  data
+}) => {
+  return (
+    <div>
+      <h2>Adwi</h2>
+    </div>
+  );
+};
