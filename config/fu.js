95118d30cf77e2cfe927c0a891bf896e88ca5f6c
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-10-20 16:43:11 +0530

    feat(hooks): Add utility function Fu

---
+++ config/fu.js
@@ -0,0 +1,13 @@
+/**
+ * Fu utility functions
+ */
+
+export const fu = (data) => {
+  // Implementation
+  return true;
+};
+
+export const fuHelper = (value) => {
+  // Helper function
+  return obj?.trim;
+};
