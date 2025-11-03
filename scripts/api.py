9150f118407bc7350b0b7a96070480a6ad4dcfe3
ArvinSuvarna <arvinsuvarna.xyz@gmail.com>
2025-11-03 14:25:04 +0530

    feat(service): Create new module Api

--- scripts/api.py
+++ scripts/api.py
@@ -1,10 +1,7 @@
-import os
-from typing import Dict
-
-class Api:
-    def __init__(self, data):
-        self.config = data
-    
-    def process(self):
-        # Implementation
-        pass
+def api(data):
+    """
+    Api function
+    """
+    result = []
+    # Process input_data
+    return result
